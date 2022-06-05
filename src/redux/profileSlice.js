f792a7d37af15443de27a301eacc5fdfa8697f9a
tundeph <54335828+tundeph@users.noreply.github.com>
2022-06-05 17:55:59 +0100

    Create profileSlice.js

---
+++ src/redux/profileSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  userid: "001",
+}
+
+export const profileSlice = createSlice({
+  name: "userProfile",
+  initialState,
+  reducers: {
+    isLoggedIn: (state, action) => {
+      state.userid = action.payload
+    },
+  },
+})
+
+export const selectUserProfile = (state) => state.userProfile
+
+export const { isLoggedIn } = profileSlice.actions
+
+export default profileSlice.reducer
