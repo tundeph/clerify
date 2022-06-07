dd838481a85eee23093840f13e2333ea42a8db9a
tundeph <54335828+tundeph@users.noreply.github.com>
2022-06-07 22:47:48 +0100

    Update App.js

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,22 +1,56 @@
-import React, { useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { isLoggedIn, selectUserProfile } from "../redux/profileSlice"
+import React from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { selectUserProfile } from "../redux/profileSlice"
+
+//pages
+import Signup from "../pages/Signup"
+import Signin from "../pages/Signin"
+import Dashboard from "../pages/Dashboard"
+import AddBusiness from "../pages/AddBusiness"
 
 const App = () => {
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
   const { userid } = useSelector(selectUserProfile)
-  const [userIdValue, setUserIdValue] = useState(0)
+  // const [userIdValue, setUserIdValue] = useState(0)
 
-  const handleChangeUser = () => {
-    dispatch(isLoggedIn(userIdValue))
-    console.log(userid, userIdValue)
-  }
+  // const handleChangeUser = () => {
+  //   dispatch(isLoggedIn(userIdValue))
+  //   console.log(userid, userIdValue)
+  // }
 
   return (
     <div className="App">
       Clerify {userid}
-      <input type="text" onChange={(e) => setUserIdValue(e.target.value)} />
-      <button onClick={handleChangeUser}>Change User </button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route
+            path="/signup"
+            element={
+              !userid ? <Signup /> : <Navigate replace to="/dashboard" />
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={userid ? <Dashboard /> : <Navigate replace to="/signin" />}
+          />
+          <Route
+            path="/signin"
+            element={
+              !userid ? <Signin /> : <Navigate replace to="/dashboard" />
+            }
+          />
+          <Route
+            path="/add-business"
+            element={
+              !userid ? <AddBusiness /> : <Navigate replace to="/signin" />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+      {/* <input type="text" onChange={(e) => setUserIdValue(e.target.value)} />
+      <button onClick={handleChangeUser}>Change User </button> */}
     </div>
   )
 }
