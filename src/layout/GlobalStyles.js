36ebf344182a61ca86f2eb48bad26e1cbfcd2a7a
tundeph <54335828+tundeph@users.noreply.github.com>
2022-06-07 22:47:58 +0100

    Update GlobalStyles.js

--- src/layout/GlobalStyles.js
+++ src/layout/GlobalStyles.js
@@ -1,7 +1,22 @@
 import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyles = createGlobalStyle`
-    body{
-        background-color: ${({ theme }) => theme.backgroundColor};
+
+@font-face {
+  font-family: "Beatrice";
+  src: local("Beatrice"), url("../assets/fonts/Beatrice.ttf") format("truetype");
+  font-weight: 800;
+}
+
+@font-face {
+  font-family: "Beatrice Bold";
+  src: local("Beatrice Bold"), url("../assets/fonts/Beatrice_Bold.ttf") format("truetype");
+  font-weight: 800;
+}
+
+body{
+        font-family: "Beatrice", Poppins, sans-serif;
+        background-color: ${({ theme }) => theme.colors.background};
     }
+
     `
