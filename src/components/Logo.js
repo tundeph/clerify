0d571bb339c8d3cc1c97314af9aac986a98b17ba
tundeph <54335828+tundeph@users.noreply.github.com>
2022-06-18 12:43:59 +0100

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,17 +1,27 @@
-import React from "react"
+import React, { useContext } from "react"
 import styled from "styled-components"
+import PropTypes from "prop-types"
 import { size } from "../layout/theme"
-// import theme from "../layout/theme"
-// console.log(theme.lightTheme.colors.foreground)
-const Text = styled.span`
-  color: ${(props) =>
-    props.reverse || (({ theme }) => theme.colors.foreground)};
+import { ThemeContext } from "styled-components"
+import { Text } from "../layout/styles"
+
+const LogoText = styled(Text)`
   font: ${size.s}em "Beatrice-Bold", sans-serif;
   font-weight: bold;
 `
 
-const Logo = () => {
-  return <Text>Clerify::</Text>
+const Logo = ({ reverse }) => {
+  const { colors } = useContext(ThemeContext)
+
+  return (
+    <LogoText color={reverse ? "#FFFFFF" : colors.foreground}>
+      Clerify::
+    </LogoText>
+  )
+}
+
+Logo.propTypes = {
+  reverse: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
 }
 
 export default Logo
