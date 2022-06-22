32ca692907edab2f523ad6ffec8cfd58a9d92c98
tundeph <54335828+tundeph@users.noreply.github.com>
2022-06-22 21:56:38 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,19 +4,12 @@ import "./index.css"
 import App from "./layout/App"
 import { Provider } from "react-redux"
 import store from "./redux/store"
-import { ThemeProvider } from "styled-components"
-import { GlobalStyles } from "./layout/GlobalStyles"
-
-import theme from "./layout/theme"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme["lightTheme"]}>
-        <GlobalStyles />
-        <App />
-      </ThemeProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 )
