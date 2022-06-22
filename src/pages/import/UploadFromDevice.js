cb223d134286ef858f40559e62fe29925e8e6828
tundeph <54335828+tundeph@users.noreply.github.com>
2022-06-22 21:57:54 +0100

    Create UploadFromDevice.js

---
+++ src/pages/import/UploadFromDevice.js
@@ -0,0 +1,56 @@
+import React from "react"
+import styled from "styled-components"
+import { size } from "../../layout/theme"
+
+import {
+  DivWrapper,
+  HalfDiv,
+  Text,
+  UploadInput,
+  Button,
+} from "../../layout/styles"
+import Select from "../../components/Select"
+
+export const CustomText = styled(Text)`
+  background-color: ${({ theme }) => theme.colors.gray100};
+  border-radius: 100px;
+  padding: ${size.xxs}rem ${size.xs}rem;
+  box-sizing: border-box;
+`
+
+const UploadFromDevice = ({
+  selectData,
+  selectValue,
+  selectOnChange,
+  uploadValue,
+  uploadOnChange,
+  onSubmit,
+}) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <DivWrapper gap={2}>
+        <Select
+          data={selectData}
+          value={selectValue}
+          onChange={selectOnChange}
+        />
+        <DivWrapper bottom={1} direction="horizontal" gap={3} align="center">
+          <HalfDiv>
+            <Text size={0.8}>Last date in account data </Text>
+          </HalfDiv>
+          <HalfDiv>
+            <CustomText size={0.8}> January 25, 2022 at 9:03am </CustomText>
+          </HalfDiv>
+        </DivWrapper>
+        <UploadInput
+          type="file"
+          value={uploadValue}
+          onChange={uploadOnChange}
+        />
+        <Button type="submit"> Upload data </Button>
+      </DivWrapper>
+    </form>
+  )
+}
+
+export default UploadFromDevice
