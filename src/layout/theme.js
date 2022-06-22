c94881ac20eea1c649e1c59353495752c4ce62b3
tundeph <54335828+tundeph@users.noreply.github.com>
2022-06-22 21:57:11 +0100

    Update theme.js

--- src/layout/theme.js
+++ src/layout/theme.js
@@ -4,7 +4,9 @@ const theme = {
       primary: "#000000",
       secondary: "#6B6A6A",
       reverse: "#FFFFFF",
-      lightGray: "#EBEBEB",
+      gray100: "#EBEBEB",
+      gray300: "#CCCCCC",
+      gray600: "#434343",
       red: "#B60101",
       foreground: "#000000",
       background: "#F6F6F6",
@@ -20,6 +22,7 @@ const theme = {
 }
 
 export const size = {
+  xxxs: 0.8,
   xxs: 1.0,
   xs: 1.4,
   s: 1.6,
@@ -27,6 +30,8 @@ export const size = {
   l: 2.4,
   xl: 3.2,
   xxl: 3.6,
+  "2xxl": 7.2,
+  "4xxl": 14.4,
 }
 
 export default theme
