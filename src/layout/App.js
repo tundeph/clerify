f8977abcbcbe1e91c861f4a4ad1a7b42252fc156
tundeph <54335828+tundeph@users.noreply.github.com>
2022-06-22 21:56:46 +0100

    Update App.js

--- src/layout/App.js
+++ src/layout/App.js
@@ -2,16 +2,32 @@ import React from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import { useSelector } from "react-redux"
 import { selectUserProfile } from "../redux/profileSlice"
+import styled, { ThemeProvider } from "styled-components"
+import { GlobalStyles } from "../layout/GlobalStyles"
+import theme from "../layout/theme"
 
 //pages
 import Signup from "../pages/Signup"
 import Signin from "../pages/Signin"
 import Dashboard from "../pages/Dashboard"
 import AddBusiness from "../pages/AddBusiness"
+import Categorise from "../pages/Categorise"
+import Sidebar from "../pages/Sidebar"
+import KeywordsEdit from "../pages/KeywordsEdit"
+import ImportAccounts from "../pages/import/ImportAccounts"
+import Reconcile from "../pages/Reconcile"
+import Reports from "../pages/reports/Reports"
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`
 
 const App = () => {
   // const dispatch = useDispatch()
   const { userid } = useSelector(selectUserProfile)
+  const { profileTheme } = useSelector(selectUserProfile)
+
   // const [userIdValue, setUserIdValue] = useState(0)
 
   // const handleChangeUser = () => {
@@ -20,38 +36,74 @@ const App = () => {
   // }
 
   return (
-    <div className="App">
-      Clerify {userid}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Signup />} />
-          <Route
-            path="/signup"
-            element={
-              !userid ? <Signup /> : <Navigate replace to="/dashboard" />
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={userid ? <Dashboard /> : <Navigate replace to="/signin" />}
-          />
-          <Route
-            path="/signin"
-            element={
-              !userid ? <Signin /> : <Navigate replace to="/dashboard" />
-            }
-          />
-          <Route
-            path="/add-business"
-            element={
-              !userid ? <AddBusiness /> : <Navigate replace to="/signin" />
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-      {/* <input type="text" onChange={(e) => setUserIdValue(e.target.value)} />
+    <ThemeProvider theme={theme[profileTheme]}>
+      <GlobalStyles />
+      <AppWrapper>
+        <BrowserRouter>
+          {!userid && <Sidebar />}
+          <Routes>
+            <Route path="/" element={<Signup />} />
+            <Route
+              path="/signup"
+              element={
+                !userid ? <Signup /> : <Navigate replace to="/dashboard" />
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                userid ? <Dashboard /> : <Navigate replace to="/signin" />
+              }
+            />
+            <Route
+              path="/signin"
+              element={
+                !userid ? <Signin /> : <Navigate replace to="/dashboard" />
+              }
+            />
+            <Route
+              path="/add-business"
+              element={
+                !userid ? <AddBusiness /> : <Navigate replace to="/signin" />
+              }
+            />
+            <Route
+              path="/categorise"
+              element={
+                !userid ? <Categorise /> : <Navigate replace to="/signin" />
+              }
+            />
+            <Route
+              path="/keywords"
+              element={
+                !userid ? <KeywordsEdit /> : <Navigate replace to="/signin" />
+              }
+            />
+
+            <Route
+              path="/imports"
+              element={
+                !userid ? <ImportAccounts /> : <Navigate replace to="/signin" />
+              }
+            />
+            <Route
+              path="/reconcile"
+              element={
+                !userid ? <Reconcile /> : <Navigate replace to="/signin" />
+              }
+            />
+            <Route
+              path="/reports"
+              element={
+                !userid ? <Reports /> : <Navigate replace to="/signin" />
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+        {/* <input type="text" onChange={(e) => setUserIdValue(e.target.value)} />
       <button onClick={handleChangeUser}>Change User </button> */}
-    </div>
+      </AppWrapper>
+    </ThemeProvider>
   )
 }
 
