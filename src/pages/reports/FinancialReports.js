f4aae7e436c16502f7d8ffab5e11d35de4c2b630
tundeph <54335828+tundeph@users.noreply.github.com>
2022-06-23 10:34:09 +0100

    Create FinancialReports.js

---
+++ src/pages/reports/FinancialReports.js
@@ -0,0 +1,41 @@
+import React, { useContext } from "react"
+import { ThemeContext } from "styled-components"
+import { size } from "../../layout/theme"
+import Select from "../../components/Select"
+
+import { DivWrapper, Text, Divider, KeywordsWrapper, DateInput } from "../../layout/styles"
+
+const FinancialReports = ({ onChange }) => {
+  const { colors } = useContext(ThemeContext)
+  const categories = [
+    { value: "", text: "Select category" },
+    { value: "rent", text: "Rent" },
+    { value: "marketing", text: "Marketing & Advertising" },
+  ]
+
+  return (
+    <DivWrapper gap={2}>
+      <DivWrapper direction="row" top={2} gap={1}>
+        <DivWrapper width={size["4xxl"]}>
+          <Select data={categories} />
+        </DivWrapper>
+        <DivWrapper direction="row" align="center" gap={0.8}>
+          <Text left={1} color={colors.gray600} size={0.8}>
+            From:
+          </Text>
+          <DateInput type="date" value="" onChange={onChange} />
+        </DivWrapper>
+        <DivWrapper direction="row" align="center" gap={0.8}>
+          <Text left={1} color={colors.gray600} size={0.8}>
+            To:
+          </Text>
+          <DateInput type="date" value="" onChange={onChange} />
+        </DivWrapper>
+      </DivWrapper>
+      <Divider />
+      <KeywordsWrapper></KeywordsWrapper>
+    </DivWrapper>
+  )
+}
+
+export default FinancialReports
