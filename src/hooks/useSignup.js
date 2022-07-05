5c3529504203c676fefa4604e020276cd0c33281
tundeph <54335828+tundeph@users.noreply.github.com>
2022-07-05 10:15:19 +0100

    Update useSignup.js

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,37 +1,50 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { authService } from "../firebase/config"
+import { useDispatch } from "react-redux"
+import { isLoggedIn } from "../redux/profileSlice"
 
 export const useSignup = () => {
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
+  const [isCancelled, setIsCancelled] = useState(false)
+  const dispatch = useDispatch()
 
-  const signup = async (email, password, displayName) => {
+  const signup = async (userEmail, userPassword, userDisplayName) => {
     setError(null)
     setIsPending(true)
 
     try {
-      const res = await authService.createUserWithEmailAndPassword(
-        email,
-        password
-      )
+      const res = await authService.createUserWithEmailAndPassword(userEmail, userPassword)
       console.log(res.user)
       if (!res) {
         throw new Error("Could not complete signup")
       }
 
       //add displayname to user
-      await res.user.updateProfile({ displayName })
+      await res.user.updateProfile({ userDisplayName })
 
-      setIsPending(false)
-      setError(null)
+      //dispatch the action to store uid in state
+      const { uid, displayName, photoURL, email } = res
+      await dispatch(isLoggedIn({ uid, displayName, photoURL, email }))
+
+      if (!isCancelled) {
+        setIsPending(false)
+        setError(null)
+      }
 
       //catch error
     } catch (err) {
-      console.log(err.message)
-      setError(err.message)
-      setIsPending(false)
+      if (!isCancelled) {
+        console.log(err.message)
+        setError(err.message)
+        setIsPending(false)
+      }
     }
   }
+
+  useEffect(() => {
+    return () => setIsCancelled(true)
+  }, [])
 
   return { error, isPending, signup }
 }
