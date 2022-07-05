4710fe78514b3daf51b9c8e976216cf6c580d3bd
tundeph <54335828+tundeph@users.noreply.github.com>
2022-07-05 10:16:48 +0100

    Update profileSlice.js

--- src/redux/profileSlice.js
+++ src/redux/profileSlice.js
@@ -1,21 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-  userid: "",
+  user: null,
+  authIsReady: true,
+  profileTheme: "lightTheme",
 }
 
 export const profileSlice = createSlice({
   name: "userProfile",
   initialState,
   reducers: {
-    isLoggedIn: (state, action) => {
-      state.userid = action.payload
+    isLoggedIn: (state, { payload }) => {
+      state.user = payload
+    },
+    authReady: (state, { payload }) => {
+      state.user = payload
+      state.authIsReady = true
+    },
+    logOut: () => {
+      return { ...initialState, authIsReady: true }
     },
   },
 })
 
 export const selectUserProfile = (state) => state.userProfile
 
-export const { isLoggedIn } = profileSlice.actions
+export const { isLoggedIn, authReady, logOut } = profileSlice.actions
 
 export default profileSlice.reducer
