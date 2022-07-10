ddcbd0bfc353faf4e4b66b5b7bf73206300390ab
tundeph <54335828+tundeph@users.noreply.github.com>
2022-07-10 14:52:48 +0100

    Update Select.js

--- src/components/Select.js
+++ src/components/Select.js
@@ -2,15 +2,24 @@ import React from "react"
 import { SelectInput } from "../layout/styles"
 
 const Select = ({ name, data, height, bgColor, fontSize, value, onChange }) => {
+  const handleValue = (val, object) => {
+    let res = ""
+    if (val) {
+      res = val
+    } else {
+      object.forEach((selectedObject) => {
+        if (selectedObject.selected) {
+          res = selectedObject.value
+        }
+      })
+    }
+
+    return res
+  }
+  const newValue = handleValue(value, data)
+
   return (
-    <SelectInput
-      height={height}
-      bgColor={bgColor}
-      fontSize={fontSize}
-      name={name}
-      onChange={onChange}
-      value={value}
-    >
+    <SelectInput height={height} bgColor={bgColor} fontSize={fontSize} name={name} onChange={onChange} value={newValue}>
       {data.map((d, i) => {
         return (
           <option key={i} value={d.value}>
