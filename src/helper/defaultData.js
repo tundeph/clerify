511f33b2efacc92632890d9240e13e9a08492474
tundeph <54335828+tundeph@users.noreply.github.com>
2022-07-10 14:52:57 +0100

    Create defaultData.js

---
+++ src/helper/defaultData.js
@@ -0,0 +1,28 @@
+export const businessType = [
+  { value: "", text: "Select type of business" },
+  { value: "Sole Proprietorship", text: "Sole Proprietorship" },
+  { value: "Partnership", text: "Partnership" },
+  { value: "Limited Liability Company", text: "Limited Liability Company" },
+  { value: "Public Liability Company", text: "Public Liability Company" },
+  { value: "Non-Profit Organization", text: "Non-Profit Organization" },
+]
+
+export const businessCategories = [
+  { value: "", text: "Select type of business" },
+  { value: "Sales Income", text: "Sales Income" },
+  { value: "Payroll", text: "Payroll" },
+  { value: "Rent or mortgage payments", text: "Rent or mortgage payments" },
+  { value: "Office supplies & expenses", text: "Office supplies & expenses" },
+  { value: "Marketing & Advertising", text: "Marketing & Advertising" },
+  { value: "Meals & Entertainment", text: "Meals & Entertainment" },
+  { value: "Furniture & equipment", text: "Furniture & equipment" },
+  { value: "Website and software expenses", text: "Website and software expenses" },
+  { value: "Employee benefits", text: "Employee benefits" },
+  { value: "Taxes", text: "Taxes" },
+  { value: "Bank charges", text: "Bank charges" },
+  { value: "Training & education", text: "Training & education" },
+  { value: "Professional Services", text: "Professional Services" },
+  { value: "Loan & Interest Payments", text: "Loan & Interest Payments" },
+  { value: "Insurance", text: "Insurance" },
+  { value: "Cost of goods", text: "Cost of goods" },
+]
