95b2158b6619f3171cd7d403cac0de52ddc6a430
tundeph <54335828+tundeph@users.noreply.github.com>
2022-07-21 01:38:17 +0100

    Update SyncFromOpenBank.js

--- src/pages/import/SyncFromOpenBank.js
+++ src/pages/import/SyncFromOpenBank.js
@@ -1,12 +1,14 @@
-import React, { useState, useContext } from "react"
+import React, { useState, useContext, useReducer } from "react"
+import { useNavigate } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
-import { selectUserProfile, selectUserBusiness } from "../../redux/profileSlice"
+import { selectUserProfile, selectUserBusiness, selectBusinessAccounts } from "../../redux/profileSlice"
+import { useFirestore } from "../../hooks/useFirestore"
 
-import { addYears, format, isAfter, isBefore } from "date-fns"
+import { addYears, format, isBefore } from "date-fns"
 import { textSorter } from "../../helper"
 
 //
-import styled, { css, ThemeContext } from "styled-components"
+import styled, { ThemeContext } from "styled-components"
 import { size } from "../../layout/theme"
 
 import {
@@ -20,11 +22,15 @@ import {
   InfoBox,
   Title,
   SubTitle,
+  LoadingIcon,
+  DottedLoadingIcon,
 } from "../../layout/styles"
 
 import Select from "../../components/Select"
+import Modal from "../../components/Modal"
 import RadioInput from "../../components/RadioInput"
 import ButtonState from "../../components/ButtonState"
+import { Navigate } from "react-router-dom"
 
 export const CustomText = styled(Text)`
   background-color: ${({ theme }) => theme.colors.gray100};
@@ -35,11 +41,10 @@ export const CustomText = styled(Text)`
 
 const SyncFromOpenBank = () => {
   const { colors } = useContext(ThemeContext)
-  const hasAccounts = null // { transDate: "2022/01/11" }
-  const { selectedBusinessId } = useSelector(selectUserProfile)
-  const business = useSelector(selectUserBusiness)
-  // const { document, error } = useDocument("business", selectedBusinessId)
-  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { addDocumentWithId, updateDocument, response } = useFirestore("accounts")
+
+  //- NEEDED --useEffect to load lastAcctData into state
 
   //
   const dateNow = new Date()
@@ -49,6 +54,41 @@ const SyncFromOpenBank = () => {
   const [endDate, setEndDate] = useState(format(dateNow, "yyyy-MM-dd"))
   const [syncDate, setSyncDate] = useState(format(dateNow, "yyyy-MM-dd"))
   const [selectedAccount, setSelectedAccount] = useState("")
+
+  //reducer for status messages in modal
+
+  const initialState = {
+    status: false,
+    mssg: {},
+  }
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "LOADING_DATA":
+        return { ...state, mssg: action.payload, status: true }
+      case "ADDING_DATA":
+        return { ...state, mssg: action.payload, status: true }
+      case "ERROR":
+        return { ...state, mssg: action.payload, status: true }
+      case "CLEAR_MODAL":
+        return { ...state, ...initialState }
+      default:
+        return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const buttonCondition = selectedAccount
+
+  // const lastAcctData = null
+  const { selectedBusinessId } = useSelector(selectUserProfile)
+  const business = useSelector(selectUserBusiness)
+  const getBusinessAccts = useSelector((state) => selectBusinessAccounts(state, selectedBusinessId))
+  const [businessAccts] = getBusinessAccts.filter((businessAcct) => businessAcct.acctName === selectedAccount)
+
+  // const { document, error } = useDocument("business", selectedBusinessId)
+  // const dispatch = useDispatch()
 
   //
   const bankAccounts = textSorter([...business[selectedBusinessId].accts], "asc", "acctName").map((acct) => ({
@@ -84,66 +124,149 @@ const SyncFromOpenBank = () => {
   }
 
   const handleSyncDateChange = (e) => {
-    if (new Date(e.target.value) > hasAccounts.transDate) {
+    if (new Date(e.target.value) > businessAccts.lastAcctData.transDate) {
       setSyncDate(e.target.value)
     } else {
-      setSyncDate(format(new Date(hasAccounts.transDate), "yyyy-MM-dd"))
+      setSyncDate(format(new Date(businessAccts.lastAcctData.transDate), "yyyy-MM-dd"))
+    }
+  }
+
+  // const handleCancelSync = (e) => {
+  //   console.log("sync cancelled ")
+  // }
+
+  const syncModalMessage = {
+    loading: {
+      title: "Sync in Progress",
+      text: (
+        <Text justify="center" color={colors.gray600}>
+          Please be patient, your data is being synchronized. Don't close this page, as this may take up to 5 mins.
+        </Text>
+      ),
+    },
+    adding: {
+      title: "Adding to Database",
+      text: (
+        <Text justify="center" color={colors.green}>
+          The your financial data is now being added to your account, please wait a bit more!
+        </Text>
+      ),
+    },
+    reconciling: {
+      title: "Reconciling accounts...",
+      text: (
+        <Text justify="center" color={colors.green}>
+          We're almost there. We are now automatically reconciling your new data with your categories.
+        </Text>
+      ),
+    },
+    error: {
+      title: "Error!",
+      text: (
+        <Text justify="center" color={colors.red}>
+          Data sync was not successful. Please retry!
+        </Text>
+      ),
+    },
+  }
+
+  const handleStartSync = async (e) => {
+    e.preventDefault()
+    dispatch({ type: "LOADING_DATA", payload: syncModalMessage.loading })
+
+    try {
+      const res = await fetch("http://localhost:3000/gtbank?_start=0&_end=10")
+      const data = await res.json()
+      console.log(data)
+      dispatch({ type: "ADDING_DATA", payload: syncModalMessage.adding })
+      //add data into 'accounts' document
+      if (!businessAccts.hasOwnProperty("lastAcctData")) {
+        await addDocumentWithId(selectedBusinessId, { data })
+      } else {
+        await updateDocument(selectedBusinessId, { data })
+      }
+      if (!response.error) {
+        await updateDocument(selectedBusinessId, { lastAcctData: data[data.length - 1] })
+
+        //reeconcile accts if lastAcctData and update modal to show process
+
+        navigate("/reconcile")
+      } else {
+        dispatch({ type: "ERROR", payload: syncModalMessage.error })
+      }
+      // await updateDocument()
+    } catch (err) {
+      console.log("Error fetching data: " + err)
+      dispatch({ type: "ERROR", payload: syncModalMessage.error })
     }
   }
 
   return (
-    <PageWrapper>
-      <UserWrapper>
-        <form>
-          <DivWrapper bottom={size.xl}>
-            <Title> Import financial records </Title>
-            <SubTitle> Sync your financial records from your bank. </SubTitle>
-          </DivWrapper>
-
-          <DivWrapper gap={2}>
-            <Select data={sortedBankAccounts} onChange={(e) => setSelectedAccount(e.target.value)} value={selectedAccount} />
-            {!hasAccounts ? (
-              <>
-                <DivWrapper>
-                  <Text left={1} color={colors.gray600} size={0.8}>
-                    Sync from{" "}
-                    {!hasAccounts && (
-                      <>
-                        from as far back as <Text bold> {` ${oneYearBackwards}`} </Text>
-                      </>
-                    )}
-                    :
-                  </Text>
-                  <DateInput type="date" value={startDate} onChange={handleStartDateChange} />
-                </DivWrapper>
-                <DivWrapper>
-                  <Text left={1} color={colors.gray600} size={0.8}>
-                    Sync up till:
-                  </Text>
-                  <DateInput type="date" value={endDate} onChange={handleEndDateChange} />
-                </DivWrapper>
-              </>
-            ) : (
-              <>
-                <DivWrapper>
+    <>
+      <PageWrapper>
+        <UserWrapper>
+          <form onSubmit={handleStartSync}>
+            <DivWrapper bottom={size.xl}>
+              <Title> Import financial records </Title>
+              <SubTitle> Sync your financial records from your bank. </SubTitle>
+            </DivWrapper>
+            <DivWrapper gap={2}>
+              <Select data={sortedBankAccounts} onChange={(e) => setSelectedAccount(e.target.value)} value={selectedAccount} />
+              {businessAccts && !businessAccts.lastAcctData ? (
+                <>
+                  <DivWrapper>
+                    <Text left={1} color={colors.gray600} size={0.8}>
+                      Sync from{" "}
+                      {!businessAccts.lastAcctData && (
+                        <>
+                          as far back as <Text bold> {` ${oneYearBackwards}`} </Text>
+                        </>
+                      )}
+                      :
+                    </Text>
+                    <DateInput type="date" value={startDate} onChange={handleStartDateChange} />
+                  </DivWrapper>
+                  <DivWrapper>
+                    <Text left={1} color={colors.gray600} size={0.8}>
+                      Sync up till:
+                    </Text>
+                    <DateInput type="date" value={endDate} onChange={handleEndDateChange} />
+                  </DivWrapper>
+                </>
+              ) : (
+                <DivWrapper bottom={2}>
                   <Text left={1} color={colors.gray600} size={0.8}>
                     Sync{" "}
-                    {hasAccounts && (
+                    {businessAccts && businessAccts.lastAcctData && (
                       <>
-                        from last date <Text bold> {new Date(hasAccounts.transDate).toDateString()} </Text>
+                        from last date <Text bold> {new Date(businessAccts.lastAcctData.transDate).toDateString()} </Text>
                       </>
                     )}
                     up till:
                   </Text>
                   <DateInput type="date" value={syncDate} onChange={handleSyncDateChange} />
                 </DivWrapper>
-              </>
+              )}
+              <ButtonState loading={false} condition={buttonCondition}>
+                Sync data
+              </ButtonState>
+            </DivWrapper>
+          </form>
+        </UserWrapper>
+      </PageWrapper>
+      {state.status && (
+        <Modal title={state.mssg.title} handleClose={() => dispatch({ type: "CLEAR_MODAL" })}>
+          <DivWrapper align="center" gap={2}>
+            {state.mssg.title !== syncModalMessage.error.title && (
+              <Text color={colors.gray300}>
+                <LoadingIcon size={80} />
+              </Text>
             )}
-            <Button type="submit">Sync data </Button>
+            {state.mssg.text}
           </DivWrapper>
-        </form>
-      </UserWrapper>
-    </PageWrapper>
+        </Modal>
+      )}
+    </>
   )
 }
 
