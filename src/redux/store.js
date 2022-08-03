f5fad76271b07b9bff2f3b8388131f3dae2423b4
tundeph <54335828+tundeph@users.noreply.github.com>
2022-08-03 03:48:20 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import profileReducer from "./profileSlice"
+import accountReducer from "./accountSlice"
 
 export default configureStore({
   reducer: {
     userProfile: profileReducer,
+    userAccts: accountReducer,
   },
 })
