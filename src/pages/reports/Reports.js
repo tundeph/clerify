737672e545f71a8c67aa394d022359d7a4845bc1
tundeph <54335828+tundeph@users.noreply.github.com>
2022-08-03 03:48:36 +0100

    Update Reports.js

--- src/pages/reports/Reports.js
+++ src/pages/reports/Reports.js
@@ -1,62 +1,28 @@
 import React from "react"
-import styled from "styled-components"
 import { size } from "../../layout/theme"
-import {
-  PageWrapper,
-  UserWrapper,
-  DivWrapper,
-  Title,
-  Text,
-} from "../../layout/styles"
+import { Outlet } from "react-router-dom"
+import { PageWrapper, UserWrapper, DivWrapper, Title, Divider } from "../../layout/styles"
 
-import TabDiv from "../../components/TabDiv"
-
-import CategoryReports from "./CategoryReports"
-import FinancialReports from "./FinancialReports"
-
-export const CustomText = styled(Text)`
-  background-color: ${({ theme }) => theme.colors.gray100};
-  border-radius: 100px;
-  padding: ${size.xxs}rem ${size.xs}rem;
-  box-sizing: border-box;
-`
-
-export const HalfDiv = styled(DivWrapper)`
-  width: 50%;
-`
+import CustomNavLink from "../../components/CustomNavLink"
 
 const Reports = () => {
-  const categories = [
-    { value: "", text: "Select account" },
-    { value: "gtb", text: "Guarantee Trust Bank" },
-    { value: "zenith", text: "Zenith Bank" },
-  ]
-
-  const Tab1 = <CategoryReports selectData={categories} />
-
-  const Tab2 = <FinancialReports selectData={categories} />
-
-  const contents = [
-    { title: "By Category", data: Tab1 },
-    { title: "By Financial Reports", data: Tab2 },
-  ]
-
-  const handleClick = (text) => {
-    console.log(text)
-  }
   return (
-    <>
-      <PageWrapper>
-        <UserWrapper>
-          <DivWrapper bottom={size.m}>
-            <Title> Reports </Title>
+    <PageWrapper>
+      <UserWrapper>
+        <DivWrapper bottom={size.xxxs}>
+          <Title> Reports </Title>
+          <DivWrapper>
+            <DivWrapper direction="row" gap={1.5} top={2}>
+              <CustomNavLink to="/reports">Visual Reports</CustomNavLink>
+              <CustomNavLink to="/reports/financial">Financial Reports</CustomNavLink>
+            </DivWrapper>
+            <Divider />
           </DivWrapper>
-          <DivWrapper bottom={size.m}>
-            <TabDiv contents={contents} />
-          </DivWrapper>
-        </UserWrapper>
-      </PageWrapper>
-    </>
+        </DivWrapper>
+
+        <Outlet />
+      </UserWrapper>
+    </PageWrapper>
   )
 }
 
