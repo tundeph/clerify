5bf9b44a5bc4e1dafe12c4824b503f7e9db94478
tundeph <54335828+tundeph@users.noreply.github.com>
2022-08-03 03:49:03 +0100

    Update SyncFromOpenBank.js

--- src/pages/import/SyncFromOpenBank.js
+++ src/pages/import/SyncFromOpenBank.js
@@ -1,36 +1,33 @@
+// NEEDED - Error handling if useeffect data fails
+//- NEEDED --maybe a useEffect to load lastAcctData into state
+
 import React, { useState, useContext, useReducer } from "react"
 import { useNavigate } from "react-router-dom"
-import { useSelector, useDispatch } from "react-redux"
-import { selectUserProfile, selectUserBusiness, selectBusinessAccounts } from "../../redux/profileSlice"
+import { useSelector } from "react-redux"
+import {
+  selectUserProfile,
+  selectUserBusiness,
+  selectBusinessAccounts,
+  selectTransactionCategories,
+} from "../../redux/profileSlice"
+
+import { useDocument } from "../../hooks/useDocument"
 import { useFirestore } from "../../hooks/useFirestore"
 
 import { addYears, format, isBefore } from "date-fns"
-import { textSorter } from "../../helper"
+import { textSorter, reconcileAccts } from "../../helper"
+import { syncModalMessages } from "../../helper/defaultData"
+import shortid from "shortid"
 
 //
 import styled, { ThemeContext } from "styled-components"
 import { size } from "../../layout/theme"
 
-import {
-  PageWrapper,
-  UserWrapper,
-  DivWrapper,
-  HalfDiv,
-  Button,
-  Text,
-  DateInput,
-  InfoBox,
-  Title,
-  SubTitle,
-  LoadingIcon,
-  DottedLoadingIcon,
-} from "../../layout/styles"
+import { PageWrapper, UserWrapper, DivWrapper, HalfDiv, Text, DateInput, Title, SubTitle, LoadingIcon } from "../../layout/styles"
 
 import Select from "../../components/Select"
 import Modal from "../../components/Modal"
-import RadioInput from "../../components/RadioInput"
 import ButtonState from "../../components/ButtonState"
-import { Navigate } from "react-router-dom"
 
 export const CustomText = styled(Text)`
   background-color: ${({ theme }) => theme.colors.gray100};
@@ -43,10 +40,9 @@ const SyncFromOpenBank = () => {
   const { colors } = useContext(ThemeContext)
   const navigate = useNavigate()
   const { addDocumentWithId, updateDocument, response } = useFirestore("accounts")
-
-  //- NEEDED --useEffect to load lastAcctData into state
-
-  //
+  const { selectedBusinessId } = useSelector(selectUserProfile)
+  const { document } = useDocument("accounts", selectedBusinessId)
+  const syncModalMessage = syncModalMessages(colors)
   const dateNow = new Date()
   const oneYearBackwards = addYears(dateNow, -1).toDateString()
 
@@ -55,8 +51,6 @@ const SyncFromOpenBank = () => {
   const [syncDate, setSyncDate] = useState(format(dateNow, "yyyy-MM-dd"))
   const [selectedAccount, setSelectedAccount] = useState("")
 
-  //reducer for status messages in modal
-
   const initialState = {
     status: false,
     mssg: {},
@@ -64,9 +58,7 @@ const SyncFromOpenBank = () => {
 
   const reducer = (state, action) => {
     switch (action.type) {
-      case "LOADING_DATA":
-        return { ...state, mssg: action.payload, status: true }
-      case "ADDING_DATA":
+      case "PROCESSING_DATA":
         return { ...state, mssg: action.payload, status: true }
       case "ERROR":
         return { ...state, mssg: action.payload, status: true }
@@ -80,19 +72,15 @@ const SyncFromOpenBank = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   const buttonCondition = selectedAccount
-
-  // const lastAcctData = null
-  const { selectedBusinessId } = useSelector(selectUserProfile)
+  //
   const business = useSelector(selectUserBusiness)
   const getBusinessAccts = useSelector((state) => selectBusinessAccounts(state, selectedBusinessId))
-  const [businessAccts] = getBusinessAccts.filter((businessAcct) => businessAcct.acctName === selectedAccount)
-
-  // const { document, error } = useDocument("business", selectedBusinessId)
-  // const dispatch = useDispatch()
+  const [businessAccts] = getBusinessAccts.filter((businessAcct) => businessAcct.id === selectedAccount)
+  const transactionCategories = useSelector((state) => selectTransactionCategories(state, selectedBusinessId))
 
   //
   const bankAccounts = textSorter([...business[selectedBusinessId].accts], "asc", "acctName").map((acct) => ({
-    value: acct.acctName,
+    value: acct.id,
     text: acct.acctName,
   }))
 
@@ -131,65 +119,43 @@ const SyncFromOpenBank = () => {
     }
   }
 
-  // const handleCancelSync = (e) => {
-  //   console.log("sync cancelled ")
-  // }
-
-  const syncModalMessage = {
-    loading: {
-      title: "Sync in Progress",
-      text: (
-        <Text justify="center" color={colors.gray600}>
-          Please be patient, your data is being synchronized. Don't close this page, as this may take up to 5 mins.
-        </Text>
-      ),
-    },
-    adding: {
-      title: "Adding to Database",
-      text: (
-        <Text justify="center" color={colors.green}>
-          The your financial data is now being added to your account, please wait a bit more!
-        </Text>
-      ),
-    },
-    reconciling: {
-      title: "Reconciling accounts...",
-      text: (
-        <Text justify="center" color={colors.green}>
-          We're almost there. We are now automatically reconciling your new data with your categories.
-        </Text>
-      ),
-    },
-    error: {
-      title: "Error!",
-      text: (
-        <Text justify="center" color={colors.red}>
-          Data sync was not successful. Please retry!
-        </Text>
-      ),
-    },
-  }
-
   const handleStartSync = async (e) => {
     e.preventDefault()
-    dispatch({ type: "LOADING_DATA", payload: syncModalMessage.loading })
+    dispatch({ type: "PROCESSING_DATA", payload: syncModalMessage.loading })
 
     try {
-      const res = await fetch("http://localhost:3000/gtbank?_start=0&_end=10")
+      const res = await fetch("http://localhost:3000/zenith?_start=0&_end=50")
       const data = await res.json()
-      console.log(data)
-      dispatch({ type: "ADDING_DATA", payload: syncModalMessage.adding })
-      //add data into 'accounts' document
-      if (!businessAccts.hasOwnProperty("lastAcctData")) {
-        await addDocumentWithId(selectedBusinessId, { data })
+
+      dispatch({ type: "PROCESSING_DATA", payload: syncModalMessage.adding })
+      // input unique id into every acount transaction adn lastAcct prop to the last accounts
+      const incomingAccounts = data.reduce((acc, item, index) => {
+        const id = shortid.generate()
+        if (index === data.length - 1) {
+          item.lastAcct = true
+        }
+        item.acctName = selectedAccount
+        item.categoryId = ""
+        acc[id] = item
+
+        return acc
+      }, {})
+
+      // add data into 'accounts' document
+      const hasLastAcctData = document
+      if (!hasLastAcctData) {
+        await addDocumentWithId(selectedBusinessId, { ...incomingAccounts })
       } else {
-        await updateDocument(selectedBusinessId, { data })
+        const updatedAccts = { ...document[selectedAccount], ...incomingAccounts }
+        await updateDocument(selectedBusinessId, { ...updatedAccts })
       }
       if (!response.error) {
-        await updateDocument(selectedBusinessId, { lastAcctData: data[data.length - 1] })
-
-        //reeconcile accts if lastAcctData and update modal to show process
-
+        // 1. update modal with syncModal.reconciling
+        dispatch({ type: "PROCESSING_DATA", payload: syncModalMessage.reconciling })
+        // 2. reconcile accts
+        const reconciledAccts = reconcileAccts(transactionCategories, incomingAccounts)
+        await updateDocument(selectedBusinessId, { ...reconciledAccts })
+        // 3. then navigate to '/reconcile' after
         navigate("/reconcile")
       } else {
         dispatch({ type: "ERROR", payload: syncModalMessage.error })
