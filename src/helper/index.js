fe36c01d15c05eb876592b8192d380282adb20f6
tundeph <54335828+tundeph@users.noreply.github.com>
2022-08-03 03:50:42 +0100

    Update index.js

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react"
 import { ThemeContext } from "styled-components"
+import { Filter } from "styled-icons/boxicons-regular"
 import { Button, DisabledButton, LoadingIcon } from "../layout/styles"
 
 export const handleButtonState = (loading, loadingText, buttonText, condition) => {
@@ -55,4 +56,82 @@ export const textSorter = (array, type, sortbyAttr) => {
   })
 
   return sorted
+}
+
+export const reconcileAccts = (categories, accounts) => {
+  if (Object.keys(categories).length === 0) return accounts
+
+  const objIn = {}
+  const objOut = {}
+
+  for (const category of categories) {
+    category.keywords.forEach((keyword) => {
+      if (category.type === "debit") {
+        objOut[keyword.toLowerCase()] = category.categoryId
+      } else {
+        objIn[keyword.toLowerCase()] = category.categoryId
+      }
+    })
+  }
+
+  Object.entries(accounts).map((account, index) => {
+    const keywords = account[1].remarks.split(/\/| |-/)
+    if (!account[1].categoryId) {
+      for (const keyword of keywords) {
+        if (account[1].type === "debit" && objOut[keyword.toLowerCase()]) {
+          accounts[account[0]].categoryId = objOut[keyword.toLowerCase()]
+          break
+        }
+
+        if (account[1].type === "credit" && objIn[keyword.toLowerCase()]) {
+          accounts[account[0]].categoryId = objIn[keyword.toLowerCase()]
+          break
+        }
+
+        if (!accounts[account[0]].hasOwnProperty("categoryId")) {
+          accounts[account[0]].categoryId = ""
+        }
+      }
+    }
+  })
+  return accounts
+}
+
+export const formatCategory = (transactionCategories) => {
+  const sortedCategories = textSorter([...transactionCategories], "asc", "title").map((category) => ({
+    value: category.categoryId,
+    text: category.title,
+  }))
+  const categories = [{ value: "", text: "Select a category" }, ...sortedCategories]
+  return categories
+}
+
+export const formatDropDown = (data, selectDescription) => {
+  const sortedData = textSorter([...new Set(data)], "asc")
+    .filter((filteredData) => filteredData.trim().length > 0)
+    .map((selectedData) => ({
+      value: selectedData,
+      text: selectedData,
+    }))
+  const finalData = [{ value: "", text: selectDescription }, ...sortedData]
+  return finalData
+}
+
+export const formatKeywordsDropDown = (uncategorisedAccts) => {
+  const keywordsArray = uncategorisedAccts.split(/\/| |-/)
+  return formatDropDown(keywordsArray, "Select a unique keyword")
+}
+
+export const formatUpdatedCategories = (document, category, keyword) => {
+  return document.categories.reduce((acc, item) => {
+    if (item.categoryId === category) {
+      if (!item.keywords.find((selectedKeyword) => selectedKeyword.toLowerCase() === keyword.toLowerCase())) {
+        item.keywords.push(keyword)
+      }
+      acc.push(item)
+    } else {
+      acc.push(item)
+    }
+    return acc
+  }, [])
 }
