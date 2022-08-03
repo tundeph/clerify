02ff7e635e460fe4eb4f67804a86271c07114a39
tundeph <54335828+tundeph@users.noreply.github.com>
2022-08-03 03:51:11 +0100

    Update Checkbox.js

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,20 +1,62 @@
 import React from "react"
-import styled from "styled-components"
-
+import styled, { css } from "styled-components"
+import { CheckmarkIcon } from "../layout/styles"
 import { size } from "../layout/theme"
 
 const CheckboxProps = styled.input.attrs({
   type: "checkbox",
 })`
-  margin-right: ${size.xxxs}rem;
+  appearance: none;
+  position: fixed;
+  z-index: 30;
+  display: block;
 `
 
-const Checkbox = ({ name, id, onChange, children }) => {
+export const SelectedProp = css`
+  color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.reverse};
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+`
+
+export const DeselectedProp = css`
+  color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.reverse};
+  &:hover {
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+  }
+`
+
+const Label = styled.label`
+  position: relative;
+  cursor: pointer;
+  padding: 0.5em 0.5em;
+`
+
+const LabelDiv = styled.div`
+  display: block;
+  resize: none;
+  cursor: pointer;
+  border-radius: 50px;
+  min-width: max-content;
+  text-align: center;
+  padding: 3px 3px;
+  border: 1px solid ${({ theme }) => theme.colors.gray300};
+  ${({ checked }) => (checked ? SelectedProp : DeselectedProp)}
+`
+
+const CustomCheckmarkIcon = styled(CheckmarkIcon)`
+  padding-left: 0.5em;
+`
+
+const Checkbox = ({ name, id, value, checked, onChange, children }) => {
   return (
-    <label htmlFor={id}>
-      <CheckboxProps id={id} name={name} onChange={onChange} />
-      {children}
-    </label>
+    <LabelDiv checked={checked}>
+      {checked && <CustomCheckmarkIcon />}
+      <Label htmlFor={id}>
+        <CheckboxProps id={id} name={name} value={value} onChange={onChange} checked={checked} />
+        {children}
+      </Label>
+    </LabelDiv>
   )
 }
 
