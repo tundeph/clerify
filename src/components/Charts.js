cb9933d2980d6271a980ee8bee887a9f795265f4
tundeph <54335828+tundeph@users.noreply.github.com>
2022-08-09 13:31:07 +0100

    Update Charts.js

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -2,50 +2,50 @@ import React from "react"
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js"
 import { Line } from "react-chartjs-2"
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+export const LineChart = ({ data }) => {
+  ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top",
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+        labels: { boxWidth: 10, boxHeight: 2 },
+      },
+      title: {
+        display: true,
+        // text: "Chart.js Line Chart",
+      },
     },
-    title: {
-      display: true,
-      text: "Chart.js Line Chart",
+    elements: {
+      line: {
+        borderWidth: 2,
+      },
+      point: {
+        pointStyle: "circle",
+        radius: 0,
+        borderWidth: 0,
+      },
     },
-  },
-  elements: {
-    line: {
-      borderWidth: 1,
-    },
-    point: {
-      pointStyle: "circle",
-      radius: 0,
-      borderWidth: 0,
-    },
-  },
-}
+  }
 
-const labels = ["January", "February", "March", "April", "May", "June", ""]
+  //   const data = {
+  //     labels,
+  //     datasets: [
+  //       {
+  //         label: "Dataset 1",
+  //         data: [100, 200, 50, 600, 400, 20, 800],
+  //         borderColor: "rgb(255, 99, 132)",
+  //         backgroundColor: "rgba(255, 99, 132, 0.5)",
+  //       },
+  //       {
+  //         label: "Dataset 2",
+  //         data: [10, 2000, 500, 60, 40, 200, 80],
+  //         borderColor: "rgb(53, 162, 235)",
+  //         // backgroundColor: "rgba(53, 162, 235, 0.5)",
+  //       },
+  //     ],
+  //   }
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: [100, 200, 50, 600, 400, 20, 800],
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      data: [10, 2000, 500, 60, 40, 200, 80],
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
-}
-export const LineChart = () => {
   return <Line options={options} data={data} />
 }
