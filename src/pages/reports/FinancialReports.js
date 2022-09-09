2fa0e53a9c8ff206c8e8de6c366f845140a2b42d
tundeph <54335828+tundeph@users.noreply.github.com>
2022-09-09 18:56:53 +0100

    Update FinancialReports.js

--- src/pages/reports/FinancialReports.js
+++ src/pages/reports/FinancialReports.js
@@ -1,39 +1,70 @@
-import React, { useContext } from "react"
-import { ThemeContext } from "styled-components"
-import { size } from "../../layout/theme"
-import Select from "../../components/Select"
+import React, { useState } from "react"
+import { useSelector } from "react-redux"
+import { selectUserProfile, selectTransactionCategories } from "../../redux/profileSlice"
+import { useCharts } from "../../hooks/useCharts"
+import { useDocument } from "../../hooks/useDocument"
 
-import { DivWrapper, Text, Divider, KeywordsWrapper, DateInput } from "../../layout/styles"
+import { format, subDays } from "date-fns"
 
-const FinancialReports = ({ onChange }) => {
-  const { colors } = useContext(ThemeContext)
-  const categories = [
-    { value: "", label: "Select category" },
-    { value: "rent", label: "Rent" },
-    { value: "marketing", label: "Marketing & Advertising" },
-  ]
+import { DivWrapper, Text, DateInput, SplitDiv } from "../../layout/styles"
+import ButtonState from "../../components/ButtonState"
+import { formatCategory } from "../../helper"
+import ReportsTable from "../../components/ReportsTable"
+
+const FinancialReports = () => {
+  const todayDate = format(new Date("2022/06/30"), "yyyy-MM-dd")
+  const thirtyDaysAgo = format(subDays(new Date("2022/06/30"), 30), "yyyy-MM-dd")
+
+  const { selectedBusinessId } = useSelector(selectUserProfile)
+  const getTransactionsDoc = useDocument("accounts", selectedBusinessId)
+  const transactionCategories = useSelector((state) =>
+    selectTransactionCategories(state, selectedBusinessId)
+  )
+  const categories = formatCategory(transactionCategories)
+  const { getData, dailyCategoryData } = useCharts()
+  //
+  const [startDate, setStartDate] = useState(thirtyDaysAgo)
+  const [endDate, setEndDate] = useState(todayDate)
+  const [showCategories] = useState(categories.map((category) => category.value))
+
+  const buttonConditionPL = startDate && endDate
+
+  const handlePL = () => {
+    getData(getTransactionsDoc.document, transactionCategories, showCategories, startDate, endDate)
+  }
 
   return (
     <DivWrapper gap={2}>
-      <DivWrapper direction="row" top={2} gap={1}>
-        <DivWrapper width={size["4xxl"]}>
-          <Select options={categories} />
-        </DivWrapper>
-        <DivWrapper direction="row" align="center" gap={0.8}>
-          <Text left={1} color={colors.gray600} size={0.8}>
-            From:
-          </Text>
-          <DateInput type="date" value="" onChange={onChange} />
-        </DivWrapper>
-        <DivWrapper direction="row" align="center" gap={0.8}>
-          <Text left={1} color={colors.gray600} size={0.8}>
-            To:
-          </Text>
-          <DateInput type="date" value="" onChange={onChange} />
-        </DivWrapper>
+      <Text bold="bold"> Profit and Loss Statement </Text>
+      <SplitDiv minWidth={120} gap={1} top={1}>
+        <DateInput
+          type="date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+          size="small"
+        />
+        <DateInput
+          type="date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+          size="small"
+        />
+        <ButtonState
+          size="small"
+          loading={false}
+          condition={buttonConditionPL}
+          loadingText=""
+          onClick={handlePL}
+        >
+          Apply
+        </ButtonState>
+      </SplitDiv>
+
+      <DivWrapper>
+        {dailyCategoryData && transactionCategories && (
+          <ReportsTable data={dailyCategoryData} categories={transactionCategories} />
+        )}
       </DivWrapper>
-      <Divider />
-      <KeywordsWrapper></KeywordsWrapper>
     </DivWrapper>
   )
 }
