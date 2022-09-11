039e3fce84a68bd51db43b7da5d99e5b5dd1824c
tundeph <54335828+tundeph@users.noreply.github.com>
2022-09-11 23:39:01 +0100

    Update index.js

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react"
 import { ThemeContext } from "styled-components"
-import { Filter } from "styled-icons/boxicons-regular"
 import { Button, DisabledButton, LoadingIcon } from "../layout/styles"
 
 export const handleButtonState = (loading, loadingText, buttonText, condition) => {
@@ -58,38 +57,55 @@ export const textSorter = (array, type, sortbyAttr) => {
   return sorted
 }
 
+export const getKeywordsFromRemarks = (remarks) => {
+  //CONSIDER FILTERING OUT GENERIC WORDS LIKE NIP, CR etc to make it smarter
+  //split into 1-word keywords
+  let keywordsArray = remarks.trim().split(/\/| |-/)
+  // add 2-word keywords
+  const twoWordKeywords = []
+  for (let counter = 0; counter <= keywordsArray.length - 1; counter++) {
+    if (keywordsArray[counter] && keywordsArray[counter + 1]) {
+      twoWordKeywords.push(`${keywordsArray[counter]} ${keywordsArray[counter + 1]}`)
+    }
+  }
+  // add 3-word keywords
+  const threeWordKeywords = []
+  for (let counter = 0; counter <= Math.abs(keywordsArray.length / 2); counter++) {
+    if (keywordsArray[counter] && keywordsArray[counter + 1] && keywordsArray[counter + 2]) {
+      threeWordKeywords.push(
+        `${keywordsArray[counter]} ${keywordsArray[counter + 1]} ${keywordsArray[counter + 2]}`
+      )
+    }
+  }
+
+  keywordsArray = [...keywordsArray, ...twoWordKeywords, ...threeWordKeywords]
+
+  return keywordsArray
+}
+
 export const reconcileAccts = (categories, accounts) => {
   if (Object.keys(categories).length === 0) return accounts
 
-  const objIn = {}
-  const objOut = {}
-
-  for (const category of categories) {
-    category.keywords.forEach((keyword) => {
-      if (category.type === "debit") {
-        objOut[keyword.toLowerCase()] = category.categoryId
-      } else {
-        objIn[keyword.toLowerCase()] = category.categoryId
-      }
-    })
-  }
-
-  Object.entries(accounts).map((account, index) => {
-    const keywords = account[1].remarks.split(/\/| |-/)
+  /// new logic
+  Object.entries(accounts).forEach((account) => {
     if (!account[1].categoryId) {
-      for (const keyword of keywords) {
-        if (account[1].type === "debit" && objOut[keyword.toLowerCase()]) {
-          accounts[account[0]].categoryId = objOut[keyword.toLowerCase()]
-          break
+      for (const category of categories) {
+        let foundCategory = false
+
+        if (category.type === account[1].type) {
+          category.keywords.forEach((selected) => {
+            if (
+              account[1].remarks.includes(selected.keyword) &&
+              account[1].remarks.includes(selected.thirdParty)
+            ) {
+              accounts[account[0]].categoryId = category.categoryId
+              foundCategory = true
+            }
+          })
         }
 
-        if (account[1].type === "credit" && objIn[keyword.toLowerCase()]) {
-          accounts[account[0]].categoryId = objIn[keyword.toLowerCase()]
+        if (foundCategory) {
           break
-        }
-
-        if (!accounts[account[0]].hasOwnProperty("categoryId")) {
-          accounts[account[0]].categoryId = ""
         }
       }
     }
@@ -97,17 +113,8 @@ export const reconcileAccts = (categories, accounts) => {
   return accounts
 }
 
-export const formatCategory = (transactionCategories) => {
-  const sortedCategories = textSorter([...transactionCategories], "asc", "title").map((category) => ({
-    value: category.categoryId,
-    label: category.title,
-  }))
-  const categories = [{ value: "", label: "Select a category" }, ...sortedCategories]
-  return categories
-}
-
-export const formatDropDown = (data, selectDescription) => {
-  const sortedData = textSorter([...new Set(data)], "asc")
+export const formatDropDown = (data, selectDescription, sortByProp) => {
+  const sortedData = textSorter([...new Set(data)], "asc", sortByProp)
     .filter((filteredData) => filteredData.trim().length > 0)
     .map((selectedData) => ({
       value: selectedData,
@@ -117,30 +124,38 @@ export const formatDropDown = (data, selectDescription) => {
   return finalData
 }
 
-export const formatKeywordsDropDown = (uncategorisedAccts) => {
-  let keywordsArray = uncategorisedAccts.trim().split(/\/| |-/)
-  // add 2-word keywords
-  const twoWordKeyWords = []
-  for (let counter = 0; counter <= keywordsArray.length - 1; counter++) {
-    if (keywordsArray[counter] && keywordsArray[counter + 1]) {
-      twoWordKeyWords.push(`${keywordsArray[counter]} ${keywordsArray[counter + 1]}`)
-    }
-  }
-  const threeWordKeyWords = []
-  for (let counter = 0; counter <= Math.abs(keywordsArray.length / 2); counter++) {
-    if (keywordsArray[counter] && keywordsArray[counter + 1] && keywordsArray[counter + 2]) {
-      threeWordKeyWords.push(`${keywordsArray[counter]} ${keywordsArray[counter + 1]} ${keywordsArray[counter + 2]}`)
-    }
-  }
-  keywordsArray = [...keywordsArray, ...twoWordKeyWords, ...threeWordKeyWords]
-  return formatDropDown(keywordsArray, "Select a keyword or phrase")
+export const formatCategory = (transactionCategories) => {
+  const sortedCategories = textSorter([...transactionCategories], "asc", "title").map(
+    (category) => ({
+      value: category.categoryId,
+      label: category.title,
+    })
+  )
+  return sortedCategories
 }
 
-export const formatUpdatedCategories = (document, category, keyword) => {
+export const formatCategoryDropDown = (transactionCategories) => {
+  const sortedCategories = formatCategory(transactionCategories)
+  const categories = [{ value: "", label: "Select a transaction category" }, ...sortedCategories]
+  return categories
+}
+
+export const formatKeywordsDropDown = (uncategorisedAccts) => {
+  const keywordsArray = getKeywordsFromRemarks(uncategorisedAccts)
+  return formatDropDown(keywordsArray, "Select ")
+}
+
+export const formatUpdatedCategories = (document, category, keyword, thirdParty) => {
   return document.categories.reduce((acc, item) => {
     if (item.categoryId === category) {
-      if (!item.keywords.find((selectedKeyword) => selectedKeyword.toLowerCase() === keyword.toLowerCase())) {
-        item.keywords.push(keyword)
+      if (
+        !item.keywords.filter(
+          (selected) =>
+            selected.keyword.toLowerCase() === keyword.toLowerCase() &&
+            selected.thirdParty.toLowerCase() === thirdParty.toLowerCase()
+        ).length
+      ) {
+        item.keywords.push({ keyword, thirdParty })
       }
       acc.push(item)
     } else {
@@ -149,3 +164,11 @@ export const formatUpdatedCategories = (document, category, keyword) => {
     return acc
   }, [])
 }
+
+export const currencyFormatter = (number) =>
+  new Intl.NumberFormat("en-NG", { style: "currency", currency: "NGN" }).format(number)
+
+export const TEST_SECRET_KEY = "test_sk_C1d7PNuoqToOVzoSXvHQ"
+export const TEST_PUBLIC_KEY = "test_pk_SirSHhAgeBBeXvK8YYcV"
+export const LIVE_SECRET_KEY = "live_sk_eFuifauPDbVczZOBr5rg"
+export const LIVE_PUBLIC_KEY = "live_pk_3mCK0Mi5y5fO45oIGOwV"
