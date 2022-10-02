294c15e91837e06ce1bf6a12814a0aea942a2bcc
tundeph <54335828+tundeph@users.noreply.github.com>
2022-10-02 19:48:54 +0100

    Update ButtonState.js

--- src/components/ButtonState.js
+++ src/components/ButtonState.js
@@ -2,20 +2,28 @@ import React, { useContext } from "react"
 import { ThemeContext } from "styled-components"
 import { Button, DisabledButton, LoadingIcon } from "../layout/styles"
 
-const ButtonState = ({ loading, loadingText, children, condition, onClick }) => {
+const ButtonState = ({ size, loading, loadingText, children, condition, onClick }) => {
   const { colors } = useContext(ThemeContext)
 
   if (loading) {
     return (
-      <DisabledButton>
+      <DisabledButton size={size}>
         {loadingText} <LoadingIcon />
       </DisabledButton>
     )
   } else {
     if (condition) {
-      return <Button onClick={onClick}> {children}</Button>
+      return (
+        <Button size={size} onClick={onClick}>
+          {children}
+        </Button>
+      )
     } else {
-      return <DisabledButton color={colors.gray600}> {children} </DisabledButton>
+      return (
+        <DisabledButton size={size} color={colors.gray600}>
+          {children}
+        </DisabledButton>
+      )
     }
   }
 }
