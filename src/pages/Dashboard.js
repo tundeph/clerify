5f60b67b96e32cb4ad46e9e0a500ae11e61e616f
tundeph <54335828+tundeph@users.noreply.github.com>
2023-01-14 19:10:06 +0000

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -8,66 +8,67 @@ import { useCharts } from "../hooks/useCharts"
 import { PieChart, BarChart } from "../components/Charts"
 import { size } from "../layout/theme"
 import {
-  PageWrapper,
-  Title,
-  UserWrapper,
-  DivWrapper,
-  SplitDiv,
-  Text,
-  Divider,
-  GraphWrapper,
-  GrayWrapper,
+	PageWrapper,
+	Title,
+	UserWrapper,
+	DivWrapper,
+	SplitDiv,
+	Text,
+	Divider,
+	GraphWrapper,
+	GrayWrapper,
 } from "../layout/styles"
 import { db } from "../firebase/config"
 
 const Dashboard = () => {
-  const endDate = format(new Date("2022/06/30"), "yyyy-MM-dd")
-  const startDate = format(subDays(new Date("2022/06/30"), 30), "yyyy-MM-dd")
-  const { getCashflowData, dailyCashflowData, combinedCashflowData } = useCharts()
+	const endDate = format(new Date("2022/06/30"), "yyyy-MM-dd")
+	const startDate = format(subDays(new Date("2022/06/30"), 30), "yyyy-MM-dd")
+	const { getCashflowData, dailyCashflowData, combinedCashflowData } =
+		useCharts()
 
-  const { selectedBusinessId } = useSelector(selectUserProfile)
-  const [rawData, setRawData] = useState()
+	const { selectedBusinessId } = useSelector(selectUserProfile)
+	const [rawData, setRawData] = useState()
 
-  useEffect(() => {
-    db.collection("accounts")
-      .doc(selectedBusinessId)
-      .onSnapshot(
-        (snapshot) => {
-          getCashflowData(snapshot.data(), startDate, endDate)
-          setRawData(snapshot.data())
-        },
-        (err) => {
-          console.log(err.message)
-        }
-      )
-  }, [])
+	useEffect(() => {
+		db.collection("accounts")
+			.doc(selectedBusinessId)
+			.onSnapshot(
+				(snapshot) => {
+					getCashflowData(snapshot.data(), startDate, endDate)
+					setRawData(snapshot.data())
+				},
+				(err) => {
+					console.log(err.message)
+				}
+			)
+	}, [])
 
-  return (
-    <PageWrapper>
-      <UserWrapper>
-        <Title> Dashboard</Title>
-        <DivWrapper top={1}>
-          <Text size={size.xxxs} bold>
-            30-day Daily Cashflow
-          </Text>
-          {dailyCashflowData && <BarChart data={dailyCashflowData} />}
-        </DivWrapper>
-        <Divider />
-        <SplitDiv minWidth={250} gap={1} top={1}>
-          <GraphWrapper>
-            {combinedCashflowData && <PieChart data={combinedCashflowData} />}
-          </GraphWrapper>
-          <GrayWrapper padding={size.xxs}>
-            <Text size={size.xxxs} bold bottom={size.xs}>
-              Latest transactions
-            </Text>
-            <Divider />
-            {rawData && <RecentTransactions data={rawData} />}
-          </GrayWrapper>
-        </SplitDiv>
-      </UserWrapper>
-    </PageWrapper>
-  )
+	return (
+		<PageWrapper>
+			<UserWrapper>
+				<Title> Dashboard</Title>
+				<DivWrapper top={1}>
+					<Text size={size.xxxs} bold>
+						30-day Daily Cashflow
+					</Text>
+					{dailyCashflowData && <BarChart data={dailyCashflowData} />}
+				</DivWrapper>
+				<Divider />
+				<SplitDiv minWidth={250} gap={1} top={1}>
+					<GraphWrapper>
+						{combinedCashflowData && <PieChart data={combinedCashflowData} />}
+					</GraphWrapper>
+					<GrayWrapper padding={size.xxs}>
+						<Text size={size.xxxs} bold bottom={size.xs}>
+							Latest transactions
+						</Text>
+						<Divider />
+						{rawData && <RecentTransactions data={rawData} />}
+					</GrayWrapper>
+				</SplitDiv>
+			</UserWrapper>
+		</PageWrapper>
+	)
 }
 
 export default Dashboard
