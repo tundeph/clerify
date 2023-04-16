0c84bd095654f820dc7cbac366d680e7439e0a58
tundeph <54335828+tundeph@users.noreply.github.com>
2023-04-16 19:53:25 +0100

    Update Sidebar.js

--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -4,101 +4,121 @@ import styled, { css } from "styled-components"
 import { useLogout } from "../hooks/useLogout"
 
 import {
-  PageWrapper,
-  DivWrapper,
-  DashboardIcon,
-  ImportAccountsIcon,
-  ReconcileIcon,
-  ReportsIcon,
-  SettingsIcon,
-  SignOutIcon,
+	PageWrapper,
+	DivWrapper,
+	DashboardIcon,
+	ImportAccountsIcon,
+	ReconcileIcon,
+	ReportsIcon,
+	SettingsIcon,
+	SignOutIcon,
 } from "../layout/styles"
 import Logo from "../components/Logo"
 import Select from "../components/Select"
 
-const width = "300"
+const WIDTH = "300"
 
 const SideBar = styled(PageWrapper)`
-  display: flex;
-  flex-direction: "row";
-  background-color: ${({ theme }) => theme.colors.foreground};
-  min-height: 100vh;
-  width: ${width}px;
-  max-width: ${width}px;
-  min-width: ${width}px;
-  padding: ${width / 6}px ${width / 6}px;
-  box-sizing: border-box;
+	display: flex;
+	flex-direction: "row";
+	background-color: ${({ theme }) => theme.colors.foreground};
+	min-height: 100vh;
+	width: ${WIDTH}px;
+	max-width: ${WIDTH}px;
+	min-width: ${WIDTH}px;
+	padding: ${WIDTH / 6}px ${WIDTH / 6}px;
+	box-sizing: border-box;
 `
 
 const SidebarContent = styled(DivWrapper)`
-  position: fixed;
-  min-width: ${width - width / 3}px;
+	position: fixed;
+	min-width: ${WIDTH - WIDTH / 3}px;
 `
 
 const NavProps = css`
-  display: flex;
-  gap: 1.2rem;
-  text-decoration: none;
-  margin: 0.5em 0;
-  color: ${({ theme }) => theme.colors.gray300};
+	display: flex;
+	align-items: center;
+	gap: 8px;
+	text-decoration: none;
+	color: ${({ theme }) => theme.colors.gray300};
+	font-size: 0.9rem;
+	border-radius: 4px;
+	padding: 8px;
+	margin-bottom: 2px;
+	background-color: ${({ theme }) => theme.colors.foreground};
 
-  &:hover {
-    filter: brightness(200%);
-    font-family: "Beatrice-Bold", sans-serif;
-  }
+	&:hover {
+		color: ${({ theme }) => theme.colors.gray400};
+		background-color: ${({ theme }) => theme.colors.gray900};
+	}
 
-  &.active {
-    filter: invert(80%);
-    font-family: "Beatrice-Bold", sans-serif;
-  }
+	&.active {
+		background-color: ${({ theme }) => theme.colors.gray900};
+	}
 `
 const Nav = styled(NavLink)`
-  ${NavProps}
+	${NavProps}
+`
+
+const SubTitle = styled.span`
+	margin-top: 15px;
+	width: 100%;
+	/* border-bottom: 1px solid white; */
+	filter: invert(50%);
+	font-family: "Beatrice-Bold", sans-serif;
+	color: ${({ theme }) => theme.colors.background};
 `
 
 const Sidebar = ({ business, onChange }) => {
-  const { logout } = useLogout()
+	const { logout } = useLogout()
 
-  let data = Object.entries(business).map((bus) => ({
-    value: bus[0],
-    label: bus[1].name,
-    selected: bus[1].selected,
-  }))
-  data = [...data, { value: "add", label: "Add another business" }]
+	const data = Object.entries(business).map((bus) => ({
+		value: bus[0],
+		label: bus[1].name,
+		selected: bus[1].selected,
+	}))
+	//  data = [...data, { value: "add", label: "Add another business" }]
 
-  return (
-    <SideBar>
-      <SidebarContent>
-        <DivWrapper bottom={2}>
-          <Logo reverse />
-        </DivWrapper>
-        <DivWrapper bottom={4}>
-          <Select options={data} onChange={onChange} size="small" mode="reverse" />
-        </DivWrapper>
-        <Nav to="/dashboard">
-          <DashboardIcon /> Dashboard
-        </Nav>
-        <Nav to="/sync-accounts">
-          <ImportAccountsIcon /> Sync Accounts
-        </Nav>
-
-        <Nav to="/reconcile">
-          <ReconcileIcon /> Categorize
-        </Nav>
-
-        <Nav to="/reports">
-          <ReportsIcon /> Reports
-        </Nav>
-
-        <Nav to="/settings">
-          <SettingsIcon /> Settings
-        </Nav>
-        <Nav to="/signin" onClick={logout}>
-          <SignOutIcon /> Sign out
-        </Nav>
-      </SidebarContent>
-    </SideBar>
-  )
+	return (
+		<SideBar>
+			<SidebarContent>
+				<DivWrapper bottom={2}>
+					<Logo reverse />
+				</DivWrapper>
+				<DivWrapper bottom={1}>
+					<Select
+						options={data}
+						onChange={onChange}
+						size="small"
+						mode="reverse"
+					/>
+				</DivWrapper>
+				<SubTitle>Home </SubTitle>
+				<Nav to="/dashboard">
+					<DashboardIcon /> Dashboard
+				</Nav>
+				<SubTitle>Synchronize </SubTitle>
+				<Nav to="/sync-accounts">
+					<ImportAccountsIcon /> Sync Accounts
+				</Nav>
+				<SubTitle>Categorize </SubTitle>
+				<Nav to="/reconcile">
+					<ReconcileIcon /> Categorize
+				</Nav>
+				<SubTitle>Reports </SubTitle>
+				<Nav to="/reports">
+					<ReportsIcon /> Reports
+				</Nav>
+				<SubTitle>Settings </SubTitle>
+				<Nav to="/settings">
+					<SettingsIcon /> Settings
+				</Nav>
+				<Nav to="/signin" onClick={logout}>
+					<SignOutIcon /> Sign out
+				</Nav>
+			</SidebarContent>
+		</SideBar>
+	)
 }
 
 export default Sidebar
