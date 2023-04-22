86ac1396f256395e1cab0f4d7dc02a0617e216bd
tundeph <54335828+tundeph@users.noreply.github.com>
2023-04-22 13:26:17 +0100

    Update Reports.js

--- src/pages/reports/Reports.js
+++ src/pages/reports/Reports.js
@@ -1,29 +1,37 @@
 import React from "react"
 import { size } from "../../layout/theme"
 import { Outlet } from "react-router-dom"
-import { PageWrapper, UserWrapper, DivWrapper, Title, Divider } from "../../layout/styles"
+import {
+	PageWrapper,
+	UserWrapper,
+	DivWrapper,
+	Title,
+	Divider,
+} from "../../layout/styles"
 
 import CustomNavLink from "../../components/CustomNavLink"
 
 const Reports = () => {
-  return (
-    <PageWrapper>
-      <UserWrapper>
-        <DivWrapper bottom={size.xxxs}>
-          <Title> Reports </Title>
-          <DivWrapper>
-            <DivWrapper direction="row" gap={1.5} top={2}>
-              <CustomNavLink to="/reports">Visual Reports</CustomNavLink>
-              <CustomNavLink to="/reports/financial">Financial Reports</CustomNavLink>
-            </DivWrapper>
-            <Divider />
-          </DivWrapper>
-        </DivWrapper>
+	return (
+		<PageWrapper>
+			<UserWrapper>
+				<DivWrapper bottom={size.xxxs}>
+					<Title> Reports </Title>
+					<DivWrapper>
+						<DivWrapper direction="column" gap={1.5} top={2}>
+							<CustomNavLink to="/reports">Visual Reports</CustomNavLink>
+							<CustomNavLink to="/reports/financial">
+								Financial Reports
+							</CustomNavLink>
+						</DivWrapper>
+						<Divider />
+					</DivWrapper>
+				</DivWrapper>
 
-        <Outlet />
-      </UserWrapper>
-    </PageWrapper>
-  )
+				{/* <Outlet /> */}
+			</UserWrapper>
+		</PageWrapper>
+	)
 }
 
 export default Reports
