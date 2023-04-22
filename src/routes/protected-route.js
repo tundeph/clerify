8206e5ed44093068b606689d699e81aa950b2ac1
tundeph <54335828+tundeph@users.noreply.github.com>
2023-04-22 13:26:35 +0100

    Update protected-route.js

--- src/routes/protected-route.js
+++ src/routes/protected-route.js
@@ -1,15 +1,35 @@
 import React from "react"
-import { Navigate, useLocation } from "react-router-dom"
+import { Route, Navigate, useLocation } from "react-router-dom"
 import { useSelector } from "react-redux"
 import { selectUserProfile } from "../redux/profileSlice"
 
-export const ProtectedRoute = ({ children }) => {
+export const ProtectedRoute = ({ children, altPath = "/signin" }) => {
 	const { user } = useSelector(selectUserProfile)
 	let location = useLocation()
 
 	if (!user) {
-		return <Navigate to="/signin" state={{ from: location }} replace />
+		return <Navigate to={altPath} state={{ from: location }} replace />
 	}
 
 	return children
+}
+
+export const renderProtectedRoutes = (routes) => {
+	const m = routes.map((route, index) => {
+		const { path, Element } = route
+
+		return (
+			<Route
+				key={index}
+				path={route.path}
+				element={
+					<ProtectedRoute altPath={route.altPath ? route.altPath : null}>
+						<Element />
+					</ProtectedRoute>
+				}
+			/>
+		)
+	})
+
+	return m
 }
