120bc3967d6c8d4a6eed20c8ab118eb9525234fa
tundeph <54335828+tundeph@users.noreply.github.com>
2023-04-22 21:24:07 +0100

    Update theme.js

--- src/layout/theme.js
+++ src/layout/theme.js
@@ -40,4 +40,19 @@ export const size = {
 	"8xxl": 28.8,
 }
 
+export const px = {
+	xxxs: 1,
+	xxs: 2,
+	xs: 4,
+	s: 8,
+	m: 12,
+	l: 16,
+	xl: 20,
+	xxl: 24,
+	"2xxl": 36,
+	"3xxl": 10.8,
+	"4xxl": 14.4,
+	"8xxl": 28.8,
+}
+
 export default theme
