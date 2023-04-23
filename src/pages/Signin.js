0bff9ca837c7ec4742bd8688a3468df1b46030c0
tundeph <54335828+tundeph@users.noreply.github.com>
2023-04-23 19:55:07 +0100

    Update Signin.js

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -3,118 +3,121 @@ import { useLogin } from "../hooks/useLogin"
 import { handleButtonState } from "../helper"
 
 //styles
-import Logo from "../components/Logo"
+import Logo from "../components/logo"
 import styled, { ThemeContext } from "styled-components"
 import { Link } from "react-router-dom"
 import {
-  PageWrapper,
-  MidWrapper,
-  DivWrapper,
-  Title,
-  SubTitle,
-  FormInput,
-  Text,
-  PasswordInput,
-  UnderFormText,
+	PageWrapper,
+	MidWrapper,
+	DivWrapper,
+	Title,
+	SubTitle,
+	FormInput,
+	Text,
+	PasswordInput,
+	UnderFormText,
 } from "../layout/styles"
-import Modal from "../components/Modal"
+import Modal from "../components/modal"
 
 const CustomMidWrapper = styled(MidWrapper)`
-  gap: 1.5rem;
+	gap: 1.5rem;
 `
 
 const Signin = () => {
-  const { colors } = useContext(ThemeContext)
-  const { error, isPending, login, resetPassword } = useLogin()
+	const { colors } = useContext(ThemeContext)
+	const { error, isPending, login, resetPassword } = useLogin()
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [resetModal, setResetModal] = useState({ status: false, text: "" })
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+	const [resetModal, setResetModal] = useState({ status: false, text: "" })
 
-  const buttonCondition = email.length > 5 && password.length > 5
-  const buttonPending = isPending && buttonCondition
+	const buttonCondition = email.length > 5 && password.length > 5
+	const buttonPending = isPending && buttonCondition
 
-  const handleResetPassword = (e) => {
-    e.preventDefault()
+	const handleResetPassword = (e) => {
+		e.preventDefault()
 
-    let text = email.length
-      ? `A link to reset your password has been sent to ${email}`
-      : "Enter an email in the email address field"
+		let text = email.length
+			? `A link to reset your password has been sent to ${email}`
+			: "Enter an email in the email address field"
 
-    if (email.length) {
-      console.log("here", email)
-      resetPassword(email)
-      if (error) {
-        text = `There's an error while trying to send the password reset link to ${email}. Click the Reset link again`
-      }
-    }
+		if (email.length) {
+			console.log("here", email)
+			resetPassword(email)
+			if (error) {
+				text = `There's an error while trying to send the password reset link to ${email}. Click the Reset link again`
+			}
+		}
 
-    setResetModal({ status: true, text })
-  }
+		setResetModal({ status: true, text })
+	}
 
-  const handleLogin = (e) => {
-    e.preventDefault()
-    login(email, password)
-  }
+	const handleLogin = (e) => {
+		e.preventDefault()
+		login(email, password)
+	}
 
-  return (
-    <>
-      <PageWrapper>
-        <CustomMidWrapper>
-          <form onSubmit={handleLogin}>
-            <DivWrapper bottom={3}>
-              <Logo />
-            </DivWrapper>
-            <DivWrapper bottom={1}>
-              <Title> Welcome back! </Title>
-              <SubTitle> Login with your email and password </SubTitle>
-            </DivWrapper>
-            <DivWrapper gap={1}>
-              <FormInput
-                type="text"
-                placeholder="Email address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <DivWrapper>
-                <PasswordInput
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <UnderFormText>
-                  Forgot password?{" "}
-                  <Link to="" onClick={handleResetPassword}>
-                    Reset it
-                  </Link>
-                </UnderFormText>
-              </DivWrapper>
-            </DivWrapper>
-            <DivWrapper top={0.2}>
-              {handleButtonState(buttonPending, "", "Login", buttonCondition)}
+	return (
+		<>
+			<PageWrapper>
+				<CustomMidWrapper>
+					<form onSubmit={handleLogin}>
+						<DivWrapper bottom={3}>
+							<Logo />
+						</DivWrapper>
+						<DivWrapper bottom={1}>
+							<Title> Welcome back! </Title>
+							<SubTitle> Login with your email and password </SubTitle>
+						</DivWrapper>
+						<DivWrapper gap={1}>
+							<FormInput
+								type="text"
+								placeholder="Email address"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+							<DivWrapper>
+								<PasswordInput
+									type="password"
+									placeholder="Password"
+									value={password}
+									onChange={(e) => setPassword(e.target.value)}
+								/>
+								<UnderFormText>
+									Forgot password?{" "}
+									<Link to="" onClick={handleResetPassword}>
+										Reset it
+									</Link>
+								</UnderFormText>
+							</DivWrapper>
+						</DivWrapper>
+						<DivWrapper top={0.2}>
+							{handleButtonState(buttonPending, "", "Login", buttonCondition)}
 
-              <UnderFormText>
-                Don’t have an account? <Link to="/signup"> Sign Up </Link>
-              </UnderFormText>
-            </DivWrapper>
-            {error && <Text color={colors.red}>{error}</Text>}
-          </form>
-        </CustomMidWrapper>
-      </PageWrapper>
+							<UnderFormText>
+								Don’t have an account? <Link to="/signup"> Sign Up </Link>
+							</UnderFormText>
+						</DivWrapper>
+						{error && <Text color={colors.red}>{error}</Text>}
+					</form>
+				</CustomMidWrapper>
+			</PageWrapper>
 
-      {resetModal.status && (
-        <Modal title="Reset Password" handleClose={() => setResetModal({ staus: false, text: "" })}>
-          <DivWrapper align="center" gap={2}>
-            <Text justify="center" color={colors.red}>
-              {resetModal.text}
-            </Text>
-            {/* <Button onClick={() => setResetModal(false)}> Send email</Button> */}
-          </DivWrapper>
-        </Modal>
-      )}
-    </>
-  )
+			{resetModal.status && (
+				<Modal
+					title="Reset Password"
+					handleClose={() => setResetModal({ staus: false, text: "" })}
+				>
+					<DivWrapper align="center" gap={2}>
+						<Text justify="center" color={colors.red}>
+							{resetModal.text}
+						</Text>
+						{/* <Button onClick={() => setResetModal(false)}> Send email</Button> */}
+					</DivWrapper>
+				</Modal>
+			)}
+		</>
+	)
 }
 
 export default Signin
