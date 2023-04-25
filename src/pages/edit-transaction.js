79dc890bc763b60411e78cb2cdfb386ebf152faf
tundeph <54335828+tundeph@users.noreply.github.com>
2023-04-25 18:22:16 +0100

    Create edit-transaction.js

---
+++ src/pages/edit-transaction.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+
+import { PageWrapper, UserWrapper, DivWrapper, Title } from "../layout/styles"
+import { size } from "../layout/theme"
+
+const EditTransaction = () => {
+	console.log("here here ooo")
+	return (
+		<>
+			<PageWrapper>
+				<UserWrapper>
+					<DivWrapper bottom={size.xxxs}>
+						<Title> Edit Transaction </Title>
+						<DivWrapper></DivWrapper>
+					</DivWrapper>
+				</UserWrapper>
+			</PageWrapper>
+		</>
+	)
+}
+
+export default EditTransaction
