43490968c42ad9c7f64d36b4ed6992344ee4b7ff
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-04 06:14:48 +0100

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,7 @@ const { alias } = require("react-app-rewire-alias")
 module.exports = function override(config) {
 	alias({
 		"@services": "src/services",
+		"@components": "src/components",
 	})(config)
 
 	return config
