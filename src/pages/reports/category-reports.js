82677ba53d1f9940de03e96cb4d55768aa0875fe
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-08 15:19:34 +0100

    Update category-reports.js

--- src/pages/reports/category-reports.js
+++ src/pages/reports/category-reports.js
@@ -4,42 +4,23 @@
 //get the chart.js graph from my exercise repo
 //if number of days is more than 30, monthly chart is spooled, if more then 365 days, yearly chart
 
-import React, { useState, useEffect } from "react"
-import { useProfileQuery } from "@services/profile-slice2"
-import { useAccountsQuery } from "../../services/account-slice"
-import { getCategoryChartData } from "../../hooks/charts-util"
-// import { useSelector } from "react-redux"
-// import {
-// 	selectUserProfile,
-// 	selectTransactionCategories,
-// } from "../../services/profile-slice"
-import { useDocument } from "../../hooks/useDocument"
-import { formatCategory, formatCategoryDropDown } from "../../helper"
+import React, { useState, useMemo } from "react"
+import { getCategoryChartData } from "../../helper/charts-util"
+import { formatCategory } from "../../helper"
 import { format, subDays } from "date-fns"
-import { renderCharts } from "../../hooks/useCharts"
-import { db } from "../../firebase/config"
 import { useOutletContext } from "react-router-dom"
 
-import Select from "../../components/select"
 import {
 	DivWrapper,
 	SplitDiv,
-	Text,
-	Divider,
 	DateInput,
-	Button,
 	GraphWrapper,
-	PageWrapper,
-	UserWrapper,
 } from "../../layout/styles"
-import { LineChart, PieChart, BarChart } from "../../components/charts"
-import ButtonState from "../../components/button-state"
+import { LineChart } from "../../components/charts"
 
 export const CategoryReports = () => {
-	const {
-		data: { business, selectedBusinessId },
-	} = useProfileQuery()
-	// const { data: accounts } = useAccountsQuery(selectedBusinessId)
+	const { selectedBusinessId, business, accounts, transactionCategories } =
+		useOutletContext()
 
 	const todayDate = format(new Date("2022/06/30"), "yyyy-MM-dd")
 	const thirtyDaysAgo = format(
@@ -50,118 +31,28 @@ export const CategoryReports = () => {
 	//
 	const [startDate, setStartDate] = useState(thirtyDaysAgo)
 	const [endDate, setEndDate] = useState(todayDate)
-	//
-	const [cashflowStartDate, setCashflowStartDate] = useState(thirtyDaysAgo)
-	const [cashflowEndDate, setCashflowEndDate] = useState(todayDate)
-	//
-	const [firstMonth, setFirstMonth] = useState(thirtyDaysAgo.substring(0, 7))
-	const [secondMonth, setSecondMonth] = useState(todayDate.substring(0, 7))
-	const [MOMCategory, setMOMCategory] = useState("")
 
-	const buttonConditionMOM = firstMonth && secondMonth && MOMCategory
-
-	const {
-		// getData,
-		// dailyCategoryData,
-		getCashflowData,
-		combinedCashflowData,
-		getMOMData,
-		MOMData,
-	} = renderCharts()
-
-	const { getData, accounts } = useOutletContext()
-
-	const transactionCategories = business[selectedBusinessId].categories
-
-	// const { selectedBusinessId } = useSelector(selectUserProfile)
-	// const transactionCategories = useSelector((state) =>
-	// 	selectTransactionCategories(state, selectedBusinessId)
-	// )
 	const categories = formatCategory(transactionCategories)
-	const getTransactionsDoc = useDocument("accounts", selectedBusinessId)
 	const [showCategories] = useState(
 		categories.map((category) => category.value)
 	)
-	const categoriesDropDown = formatCategoryDropDown(transactionCategories)
 
-	// testing ground
-	let chartData
-	if (accounts) {
-		// console.log(
-		// 	accounts,
-		// 	business[selectedBusinessId].categories,
-		// 	showCategories,
-		// 	thirtyDaysAgo,
-		// 	todayDate
-		// )
-
-		chartData = getCategoryChartData(
-			accounts,
-			business[selectedBusinessId].categories,
-			showCategories,
-			startDate,
-			endDate
-		)
-	}
-	// end of testing
-
-	useEffect(() => {
-		// db.collection("accounts")
-		// 	.doc(selectedBusinessId)
-		// 	.onSnapshot(
-		// 		(snapshot) => {
-		// 			if (snapshot.data()) {
-		// 				getData(
-		// 					snapshot.data(),
-		// 					transactionCategories,
-		// 					showCategories,
-		// 					startDate,
-		// 					endDate
-		// 				)
-		// 			}
-		// 		},
-		// 		(err) => {
-		// 			console.log(err.message)
-		// 		}
-		// 	)
-		// getData(accounts, transactionCategories, showCategories, startDate, endDate)
-	}, [])
-
-	const handleCategoryChart = () => {
-		getData(
-			getTransactionsDoc.document,
-			transactionCategories,
-			showCategories,
-			startDate,
-			endDate
-		)
-	}
-
-	const handleMOMChart = () => {
-		getMOMData(
-			getTransactionsDoc.document,
-			transactionCategories,
-			MOMCategory,
-			firstMonth,
-			secondMonth
-		)
-	}
-
-	const handleCashflowChart = () => {
-		getCashflowData(
-			getTransactionsDoc.document,
-			cashflowStartDate,
-			cashflowEndDate
-		)
-	}
+	//get the chart data
+	const chartData = useMemo(
+		() =>
+			getCategoryChartData(
+				accounts,
+				business[selectedBusinessId].categories,
+				showCategories,
+				startDate,
+				endDate
+			),
+		[accounts, showCategories, startDate, endDate]
+	)
 
 	return (
 		<>
-			{/* <SubTitle>Select the categories you want to generate reports for.</SubTitle> */}
 			<DivWrapper bottom={2}>
-				{/* <Text bold size={1}>
-					Category Reports
-				</Text> */}
 				<SplitDiv minWidth={120} gap={1} top={2}>
 					<DateInput
 						type="date"
@@ -175,9 +66,6 @@ export const CategoryReports = () => {
 						onChange={(e) => setEndDate(e.target.value)}
 						size="small"
 					/>
-					{/* <Button size="small" onClick={handleCategoryChart}>
-						Apply
-					</Button> */}
 				</SplitDiv>
 				<DivWrapper top={1}>
 					<GraphWrapper top={1}>
@@ -187,77 +75,6 @@ export const CategoryReports = () => {
 					</GraphWrapper>
 				</DivWrapper>
 			</DivWrapper>
-
-			{/* <Divider />
-			<DivWrapper top={2} bottom={2}>
-				<Text bold size={1}>
-					Cashflow Reports
-				</Text>
-				<SplitDiv minWidth={120} gap={1} top={2}>
-					<DateInput
-						type="date"
-						value={cashflowStartDate}
-						onChange={(e) => setCashflowStartDate(e.target.value)}
-						size="small"
-					/>
-					<DateInput
-						type="date"
-						value={cashflowEndDate}
-						onChange={(e) => setCashflowEndDate(e.target.value)}
-						size="small"
-					/>
-					<Button size="small" onClick={handleCashflowChart}>
-						Apply
-					</Button>
-				</SplitDiv>
-				<DivWrapper top={1}>
-					<GraphWrapper top={1}>
-						{combinedCashflowData && <PieChart data={combinedCashflowData} />}
-					</GraphWrapper>
-				</DivWrapper>
-			</DivWrapper> */}
-
-			{/* <Divider />
-
-			<DivWrapper top={2} bottom={2}>
-				<Text bold size={1}>
-					Month-on-Month (MOM) Reports
-				</Text>
-				<SplitDiv minWidth={120} gap={1} top={2}>
-					<DateInput
-						type="month"
-						value={firstMonth}
-						onChange={(e) => setFirstMonth(e.target.value)}
-						size="small"
-					/>
-					<DateInput
-						type="month"
-						value={secondMonth}
-						onChange={(e) => setSecondMonth(e.target.value)}
-						size="small"
-					/>
-					<Select
-						options={categoriesDropDown}
-						onChange={(e) => setMOMCategory(e.target.value)}
-						value={MOMCategory}
-						size="small"
-					/>
-					<ButtonState
-						size="small"
-						loading={false}
-						condition={buttonConditionMOM}
-						loadingText=""
-						onClick={handleMOMChart}
-					>
-						Apply
-					</ButtonState>
-				</SplitDiv>
-				<DivWrapper top={1}>
-					<GraphWrapper top={1}>
-						{combinedCashflowData && <BarChart data={MOMData} />}
-					</GraphWrapper>
-				</DivWrapper>
-			</DivWrapper> */}
 		</>
 	)
 }
