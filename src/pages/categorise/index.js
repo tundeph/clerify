e1d5d93affb6824f57ec3ce2de8c9d18c39d1b20
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-08 15:19:12 +0100

    Update index.js

--- src/pages/categorise/index.js
+++ src/pages/categorise/index.js
@@ -1,18 +1,14 @@
-// load transactions with useeffect
+// load transactions
 //sort only transactions with no categoryId
 // create a state for array index starting from 0
-// create a state array for credit
-// create a state array for debit
-// show array [arrayindex]
 //after user clicks NEXT update state array for credit or debit with keyword
 //loop through array, if transaction is debit, check if its keyword exist in debit vice versa
 // if it does not exist, update array index
-//NEED: Change dropdown for keywords to checkboxes component, adn investigate adding array to keywords in db and state
 
-import React, { useState, useEffect, useContext } from "react"
+import React, { useState, useContext } from "react"
 import styled, { ThemeContext } from "styled-components"
 import { clone } from "ramda"
-// import { useDocument } from "../../hooks/useDocument"
+
 import {
 	useProfileQuery,
 	useUpdateCategoriesMutation,
@@ -21,8 +17,7 @@ import {
 	useAccountsQuery,
 	useUpdateAccountsMutation,
 } from "@services/account-slice"
-// import { useFirestore } from "../../hooks/useFirestore"
-// import { db } from "../../firebase/config"
+
 import {
 	formatCategoryDropDown,
 	formatKeywordsDropDown,
@@ -37,22 +32,21 @@ import {
 	UserWrapper,
 	DivWrapper,
 	Title,
-	SubTitle,
 	CalendarIcon,
 	DebitIcon,
 	CreditIcon,
 	Text,
 	ArrowForwardIcon,
-	BouncingCabinetIcon,
 	GrayWrapper,
 	Card,
-	Divider,
-	Button,
 } from "../../layout/styles"
 
 import Select from "@components/select"
 import ButtonState from "@components/button-state"
 import NoTransactions from "@components/no-transactions"
+
+import CategoriseIntro from "./categorise-intro"
+import ReconcileCard from "./reconcile-card"
 
 export const CustomDivWrapper = styled(Card)`
 	gap: ${size.xxxs}rem;
@@ -64,25 +58,23 @@ const CustomButton = styled(ButtonState)`
 `
 
 export const CategoriseTransaction = () => {
+	//gets transactions from the redux app state
 	const {
 		data: { business, selectedBusinessId },
 	} = useProfileQuery()
 	const { data: transactions, isLoading: getAccountsLoading } =
 		useAccountsQuery(selectedBusinessId)
-	const [updateAccounts, { isLoading: accountsLoading }] =
-		useUpdateAccountsMutation()
+	const [updateAccounts] = useUpdateAccountsMutation()
 
 	let sorted = []
 
+	// this filters for only uncategorized transactions
 	if (transactions) {
 		sorted = transactions.filter(
 			(transaction) => transaction.categoryId.trim() === ""
 		)
 	}
-	const [
-		updateCategories,
-		{ isLoading: categoryLoading, error: categoryError },
-	] = useUpdateCategoriesMutation()
+	const [updateCategories] = useUpdateCategoriesMutation()
 
 	const { colors } = useContext(ThemeContext)
 	const [category, setCategory] = useState()
@@ -90,7 +82,7 @@ export const CategoriseTransaction = () => {
 
 	const [isPending, setIsPending] = useState(false)
 	const [index, setIndex] = useState(0)
-	// const [loading, setLoading] = useState(true)
+
 	const [chosenThirdParty, setChosenThirdParty] = useState("")
 	const [categorizedQty, setCategorizedQty] = useState(1)
 	const [showIntro, setShowIntro] = useState(true)
@@ -102,31 +94,19 @@ export const CategoriseTransaction = () => {
 
 	const transactionCategories = business[selectedBusinessId].categories
 	const categoriesForDropDown = formatCategoryDropDown(transactionCategories)
-	// const transactionsDb = useFirestore("accounts")
-	// const businessDb = useFirestore("business")
-	// const getTransactionsDoc = useDocument("accounts", selectedBusinessId)
-	// const getBusinessDoc = useDocument("business", selectedBusinessId)
 
 	const handleReconcile = async () => {
 		setIsPending(true)
-		//do reconciliation
 
-		// const reconciledAccts = reconcileAccts(
-		// 	transactionCategories,
-		// 	getTransactionsDoc.document
-		// )
 		const reconciledAccts = reconcileAccts(
 			transactionCategories,
 			clone(transactions)
 		)
 		setIndex(0)
-		//update transactions db
-		// await transactionsDb.updateDocument(selectedBusinessId, reconciledAccts)
-		updateAccounts({ selectedBusinessId, reconciledAccts })
 
+		updateAccounts({ selectedBusinessId, reconciledAccts })
 		setCategorizedQty(1)
 		setIsPending(false)
-		// dispatch(getTransactions({ data: reconciled }))
 	}
 
 	const handleCategorise = async () => {
@@ -160,26 +140,12 @@ export const CategoriseTransaction = () => {
 					<DivWrapper bottom={size.xs}>
 						<Title> Reconcile records </Title>
 					</DivWrapper>
-					{/* the page first shows a summary of transactions to be reconciled */}
+					{/* introcard  shows a summary of transactions to be reconciled */}
 					{showIntro && sorted.length > 0 && (
-						<Card>
-							<Text size={size.xs} bold>
-								{`Well, you still have ${sorted.length}  uncategorized transactions`}
-							</Text>
-
-							<Text size={size.xxs} bottom={1}>
-								You can reconcile all your transactions by categorising few of
-								them and then allowing the app to do the rest for you
-							</Text>
-							<Divider />
-							<Text bottom={1}>
-								Would you like to manually categorize some of your transactions
-								now?
-							</Text>
-							<Button onClick={() => setShowIntro(!showIntro)}>
-								Categorize now
-							</Button>
-						</Card>
+						<CategoriseIntro
+							sorted={sorted}
+							onClick={() => setShowIntro(!showIntro)}
+						/>
 					)}
 					{/* when Categorize now button is clicked and there are uncategorized transactions, the trnsactions show one after another */}
 					{!showIntro && sorted.length > 0 && (
@@ -295,22 +261,10 @@ export const CategoriseTransaction = () => {
 								)}
 							{sorted.length === 0 && !isPending && <NoTransactions />}
 							{showReconcileButton && (
-								<DivWrapper align="center" gap={2}>
-									<Text color={colors.gray300}>
-										<BouncingCabinetIcon size={80} />
-									</Text>
-									<Text align="center">
-										Click the button below to reconcile your transactions{" "}
-									</Text>
-									<ButtonState
-										loading={isPending}
-										condition={true}
-										loadingText=""
-										onClick={() => handleReconcile()}
-									>
-										Reconcile
-									</ButtonState>
-								</DivWrapper>
+								<ReconcileCard
+									isPending={isPending}
+									onClick={() => handleReconcile()}
+								/>
 							)}
 						</GrayWrapper>
 					)}
