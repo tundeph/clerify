c8c407231e97db388d650d6fa51b47a6a649244b
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-08 15:19:23 +0100

    Update Dashboard.js

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
+import React, { useMemo } from "react"
+
+import { useProfileQuery } from "../services/profile-slice2"
+import { useAccountsQuery } from "../services/account-slice"
+import { getCashflowChartData } from "../helper/charts-util"
 
 import { format, subDays } from "date-fns"
 import RecentTransactions from "../components/recent-transactions"
-import { renderCharts } from "../hooks/useCharts"
 import { PieChart, BarChart } from "../components/charts"
 import { size } from "../layout/theme"
 import {
@@ -17,70 +19,47 @@ import {
 	GraphWrapper,
 	GrayWrapper,
 } from "../layout/styles"
-import { db } from "../firebase/config"
-import { useProfileQuery } from "../services/profile-slice2"
 
 export const Dashboard = () => {
-	const navigate = useNavigate()
 	const { data } = useProfileQuery(undefined, {
 		refetchOnMountOrArgChange: true,
 	})
-
-	// const hasBusiness =
-	// 	data.user.hasOwnProperty("business") &&
-	// 	Object.keys(data.user.business).length
-	// if (!hasBusiness) {
-	// 	navigate("/add-business")
-	// }
+	const { data: accounts } = useAccountsQuery(data.selectedBusinessId)
 	const endDate = format(new Date("2022/06/30"), "yyyy-MM-dd")
 	const startDate = format(subDays(new Date("2022/06/30"), 30), "yyyy-MM-dd")
-	const { getCashflowData, dailyCashflowData, combinedCashflowData } =
-		renderCharts()
 
-	// const { selectedBusinessId } = data
-	const [rawData, setRawData] = useState()
-
-	useEffect(() => {
-		// const generateReports = (selectedBusinessId) => {
-		if (data) {
-			db.collection("accounts")
-				.doc(data.selectedBusinessId)
-				.onSnapshot(
-					(snapshot) => {
-						getCashflowData(snapshot.data(), startDate, endDate)
-						setRawData(snapshot.data())
-					},
-					(err) => {
-						console.log(err.message)
-					}
-				)
-		}
-		// }
-	}, [])
+	//calculate data for chart and feed into the chart components
+	let chartData
+	chartData = useMemo(
+		() => getCashflowChartData(accounts, startDate, endDate),
+		[accounts]
+	)
 
 	return (
 		<PageWrapper>
 			{data && (
 				<UserWrapper>
-					{/* {data && generateReports(data.selectedBusinessId)} */}
 					<Title> Dashboard</Title>
 					<DivWrapper top={1}>
 						<Text size={size.xxxs} bold>
 							30-day Daily Cashflow
 						</Text>
-						{dailyCashflowData && <BarChart data={dailyCashflowData} />}
+
+						{chartData && <BarChart data={chartData.resultForDailyCashflow} />}
 					</DivWrapper>
 					<Divider />
 					<SplitDiv minWidth={250} gap={1} top={1}>
 						<GraphWrapper>
-							{combinedCashflowData && <PieChart data={combinedCashflowData} />}
+							{chartData && (
+								<PieChart data={chartData.resultForMonthlyCashflow} />
+							)}
 						</GraphWrapper>
 						<GrayWrapper padding={size.xxs}>
 							<Text size={size.xxxs} bold bottom={size.xs}>
 								Latest transactions
 							</Text>
 							<Divider />
-							{rawData && <RecentTransactions data={rawData} />}
+							{accounts && <RecentTransactions data={accounts} />}
 						</GrayWrapper>
 					</SplitDiv>
 				</UserWrapper>
