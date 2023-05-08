6075cd9e87fba5aa5885c5fef0dc53304c334cff
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-08 21:38:21 +0100

    Update profile-slice2.js

--- src/services/profile-slice2.js
+++ src/services/profile-slice2.js
@@ -1,5 +1,5 @@
 import { api } from "./api"
-import { db, authService } from "../firebase/config"
+import { db, authService, timestamp } from "../firebase/config"
 import { transformLoginData } from "./utils"
 
 //an initial state is defined that is fed into global Theme object
@@ -57,7 +57,7 @@ export const authApi = api.injectEndpoints({
 					// new Promise is used to get data from the firebase function
 					// as there are several callbacks, resolve saves the response
 					// resolve is defined here because a result must be returned
-					const result = await new Promise((resolve, reject) =>
+					const result = await new Promise((resolve) =>
 						db
 							.collection("business")
 							.doc(selectedBusinessId)
@@ -77,7 +77,57 @@ export const authApi = api.injectEndpoints({
 			},
 			invalidatesTags: ["Profile"],
 		}),
+
+		addBusiness: build.mutation({
+			queryFn: async (body) => {
+				const { selectedBusinessId, business } = body
+
+				try {
+					// new Promise is used to get data from the firebase function
+					// as there are several callbacks, resolve saves the response
+					// resolve is defined here because a result must be returned
+
+					const createdAt = timestamp.fromDate(new Date())
+					const result = await new Promise((resolve) =>
+						db
+							.collection("business")
+							.doc(selectedBusinessId)
+							.add({ ...business, createdAt })
+							.then(() => resolve("success"))
+							.catch((error) => {
+								if (error) {
+									throw error
+								}
+							})
+					)
+					return { data: result }
+				} catch (err) {
+					// returns error if there is any error in the endpoint
+					return { error: err }
+				}
+			},
+			invalidatesTags: ["Profile"],
+		}),
+
+		logout: build.mutation({
+			queryFn: async () => {
+				try {
+					// use firebase function to sign out
+					const result = authService.signOut()
+					return { data: result }
+				} catch (err) {
+					// returns error if there is any error in the endpoint
+					return { error: err }
+				}
+			},
+			invalidatesTags: ["Profile"],
+		}),
 	}),
 })
 
-export const { useProfileQuery, useUpdateCategoriesMutation } = authApi
+export const {
+	useProfileQuery,
+	useUpdateCategoriesMutation,
+	useAddBusinessMutation,
+	useLogoutMutation,
+} = authApi
