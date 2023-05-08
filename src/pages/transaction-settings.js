6b45c54652cc22e32673b7fabb32f967e32abd05
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-08 15:19:57 +0100

    Update transaction-settings.js

--- src/pages/transaction-settings.js
+++ src/pages/transaction-settings.js
@@ -1,10 +1,22 @@
 import React from "react"
-import { NavLink } from "react-router-dom"
 
-import { PageWrapper, UserWrapper, DivWrapper, Title } from "../layout/styles"
+import { useProfileQuery } from "../services/profile-slice2"
+import { useAccountsQuery } from "../services/account-slice"
+
+import { NavLink } from "react-router-dom"
+import {
+	PageWrapper,
+	UserWrapper,
+	DivWrapper,
+	Title,
+	Text,
+} from "../layout/styles"
 import { size } from "../layout/theme"
 
 export const TransactionSettings = () => {
+	const { data } = useProfileQuery()
+	const { data: accounts } = useAccountsQuery(data.selectedBusinessId)
+
 	console.log("here here ooo")
 	return (
 		<>
@@ -12,6 +24,11 @@ export const TransactionSettings = () => {
 				<UserWrapper>
 					<DivWrapper bottom={size.xxxs}>
 						<Title> Edit Transaction </Title>
+						{accounts
+							? accounts
+									.filter((account, index) => index < 10)
+									.map((account) => <Text>{account.remarks} </Text>)
+							: null}
 						<DivWrapper></DivWrapper>
 					</DivWrapper>
 				</UserWrapper>
