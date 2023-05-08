4c31356a663d6719802bce6394dc6d239192c814
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-08 15:19:18 +0100

    Create reconcile-card.js

---
+++ src/pages/categorise/reconcile-card.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react"
+import { ThemeContext } from "styled-components"
+
+import { DivWrapper, Text, BouncingCabinetIcon } from "../../layout/styles"
+import ButtonState from "@components/button-state"
+
+const ReconcileCard = ({ isPending, onClick }) => {
+	const { colors } = useContext(ThemeContext)
+
+	return (
+		<DivWrapper align="center" gap={2}>
+			<Text color={colors.gray300}>
+				<BouncingCabinetIcon size={80} />
+			</Text>
+			<Text align="center">
+				Click the button below to reconcile your transactions
+			</Text>
+			<ButtonState
+				loading={isPending}
+				condition={true}
+				loadingText=""
+				onClick={onClick}
+			>
+				Reconcile
+			</ButtonState>
+		</DivWrapper>
+	)
+}
+
+export default ReconcileCard
