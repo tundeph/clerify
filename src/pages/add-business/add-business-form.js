72d8be4ad0d2f5a3ad6ef55b244217bf4c48768b
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-14 00:43:23 +0100

    Update add-business-form.js

--- src/pages/add-business/add-business-form.js
+++ src/pages/add-business/add-business-form.js
@@ -1,34 +1,15 @@
-import React, { useContext } from "react"
+import React from "react"
 
-import { ThemeContext } from "styled-components"
 import { size } from "../../layout/theme"
-import { AddFormButton, DeleteFormButton } from "../../components/form-buttons"
-import {
-	DivWrapper,
-	Divider,
-	SubTitle,
-	Text,
-	FormInput,
-} from "../../layout/styles"
+
+import { DivWrapper, SubTitle, FormInput } from "../../layout/styles"
 import Select from "../../components/select"
 
-const AddBusinessForm = ({
-	name,
-	setName,
-	type,
-	setType,
-	businessType,
-	accts,
-	handleAcctChange,
-	handleDeleteForm,
-	handleAddMore,
-}) => {
-	const { colors } = useContext(ThemeContext)
-
+const AddBusinessForm = ({ name, setName, type, setType, businessType }) => {
 	return (
 		<DivWrapper>
 			<DivWrapper gap={size.xxs}>
-				<SubTitle> Details of Business </SubTitle>
+				<SubTitle> Add a business </SubTitle>
 				<FormInput
 					onChange={(e) => setName(e.target.value)}
 					value={name}
@@ -41,45 +22,6 @@ const AddBusinessForm = ({
 					value={type}
 					options={businessType}
 				/>
-			</DivWrapper>
-			<Divider gap={size.m} />
-			<DivWrapper gap={size.xxs}>
-				<SubTitle>Add Your Financial Accounts </SubTitle>
-				<Text align="center" size={size.xxxs} color={colors.secondary}>
-					For example, different bank accounts
-				</Text>
-				{accts.map((acct, i) => {
-					return (
-						<DivWrapper key={acct.id}>
-							<Text
-								align="left"
-								left={size.xxs}
-								size={size.xxxs}
-								color={colors.secondary}
-							>
-								Account {i + 1}:
-							</Text>
-							<DivWrapper direction="row">
-								<FormInput
-									onChange={(e) => handleAcctChange(e, acct.id)}
-									value={acct.acctName}
-									type="text"
-									placeholder="e.g. ABC Bank "
-								/>
-								{accts.length > 1 && i < accts.length - 1 && (
-									<DeleteFormButton
-										onClick={handleDeleteForm}
-										size={0.7}
-										value={acct.id}
-									/>
-								)}
-								{accts.length - 1 === i && (
-									<AddFormButton onClick={handleAddMore} size={0.7} />
-								)}
-							</DivWrapper>
-						</DivWrapper>
-					)
-				})}
 			</DivWrapper>
 		</DivWrapper>
 	)
