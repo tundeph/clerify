aaf0bcfdb00307203ba80a1915cc0f28d0f7d8f2
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-14 00:44:53 +0100

    Update utils.js

--- src/services/utils.js
+++ src/services/utils.js
@@ -2,21 +2,24 @@ export const transformLoginData = (data, user, initialState) => {
 	let business = {}
 	let selectedBusinessId
 	let hasAccts = {}
+	let permission = null
 
 	const { uid, displayName, photoURL, email } = user
 
 	data.docs.forEach((doc) => {
-		const { accts, name, type, selected, categories, lastAcctData } = doc.data()
+		const { accts, name, type, selected, categories, lastAcctData, users } =
+			doc.data()
 		if (selected) {
 			selectedBusinessId = doc.id
 			hasAccts = lastAcctData
 		}
 		business[doc.id] = { id: doc.id, accts, name, type, selected, categories }
+		permission = users.filter((user) => user.uid === uid)[0].permission
 	})
 
 	return {
 		...initialState,
-		user: { uid, displayName, photoURL, email },
+		user: { uid, displayName, photoURL, email, permission },
 		business,
 		selectedBusinessId,
 		lastAcctData: hasAccts,
