adcffb34d7ca6045f0aa443d99f8b327c5b59b5a
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-14 00:42:15 +0100

    Update select.js

--- src/components/select.js
+++ src/components/select.js
@@ -1,34 +1,60 @@
 import React from "react"
-import { SelectInput } from "../layout/styles"
+import { SelectInput, Label, DivWrapper } from "../layout/styles"
 
-const Select = ({ name, options, size, bgColor, fontSize, value, onChange, mode }) => {
-  const handleValue = (val, object) => {
-    let res = ""
-    if (val) {
-      res = val
-    } else {
-      object.forEach((selectedObject) => {
-        if (selectedObject.selected) {
-          res = selectedObject.value
-        }
-      })
-    }
+const Select = ({
+	name,
+	label,
+	options,
+	size,
+	bgColor,
+	fontSize,
+	value,
+	onChange,
+	mode,
+}) => {
+	const handleValue = (val, object) => {
+		let res = ""
+		if (val) {
+			res = val
+		} else {
+			object.forEach((selectedObject) => {
+				if (selectedObject.selected) {
+					res = selectedObject.value
+				}
+			})
+		}
 
-    return res
-  }
-  const newValue = handleValue(value, options)
+		return res
+	}
+	const newValue = handleValue(value, options)
 
-  return (
-    <SelectInput size={size} bgColor={bgColor} fontSize={fontSize} name={name} onChange={onChange} value={newValue} mode={mode}>
-      {options.map((option, i) => {
-        return (
-          <option key={i} value={option.value}>
-            {option.label}
-          </option>
-        )
-      })}
-    </SelectInput>
-  )
+	return (
+		<DivWrapper>
+			{label ? (
+				<Label htmlFor={name} bottom={0.5}>
+					{label}
+				</Label>
+			) : null}
+			<SelectInput
+				size={size}
+				bgColor={bgColor}
+				fontSize={fontSize}
+				id={name}
+				name={name}
+				onChange={onChange}
+				value={newValue}
+				mode={mode}
+			>
+				{options.map((option, i) => {
+					return (
+						<option key={i} value={option.value}>
+							{option.label}
+						</option>
+					)
+				})}
+			</SelectInput>
+		</DivWrapper>
+	)
 }
 
 export default Select
