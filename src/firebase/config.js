5b52f18be3f7dab6f501e1649b594d75fcf8c64b
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-20 16:06:00 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import "firebase/firestore"
 import "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgBIBUq-399jMTZTlbTY1J8TIR8rfar3Q",
-  authDomain: "clerify-6f807.firebaseapp.com",
-  projectId: "clerify-6f807",
-  storageBucket: "clerify-6f807.appspot.com",
-  messagingSenderId: "1049325448407",
-  appId: "1:1049325448407:web:e6fb8c02a5ce5a6d42d492",
+	apiKey: "AIzaSyAgBIBUq-399jMTZTlbTY1J8TIR8rfar3Q",
+	authDomain: "clerify-6f807.firebaseapp.com",
+	projectId: "clerify-6f807",
+	storageBucket: "clerify-6f807.appspot.com",
+	messagingSenderId: "1049325448407",
+	appId: "1:1049325448407:web:e6fb8c02a5ce5a6d42d492",
 }
 
 //init firebase
@@ -21,4 +21,7 @@ const authService = firebase.auth()
 //timestamp
 const timestamp = firebase.firestore.Timestamp
 
-export { db, authService, timestamp }
+//FieldValue
+const FieldValue = firebase.firestore.FieldValue
+
+export { db, authService, timestamp, FieldValue }
