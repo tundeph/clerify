55075b2fdce7d44cdff00180268006953a008768
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-26 08:55:52 +0100

    Update delete-user-account.js

--- src/pages/settings/delete-user-account.js
+++ src/pages/settings/delete-user-account.js
@@ -1,5 +1,96 @@
-import React from "react"
+import React, { useState, useContext } from "react"
+import { ThemeContext } from "styled-components"
+import { map, prop, applySpec, prepend, pipe, reject, propEq } from "ramda"
+import {
+	useGetUsersQuery,
+	useDeleteUserMutation,
+} from "../../services/users-slice"
 
-export const DeleteUserAccount = () => {
-	return <div>DeleteUserAccount</div>
+import { DivWrapper, Text, Button } from "../../layout/styles"
+import Select from "@components/select"
+import ButtonState from "@components/button-state"
+import Modal from "@components/modal"
+
+//convert the users array to the array for the select doropdown
+const usersDropDown = (data, email) =>
+	pipe(
+		reject(propEq(email, "email")),
+		map(
+			applySpec({
+				label: prop("email"),
+				value: prop("email"),
+			})
+		),
+		prepend({ label: "Select a user", value: "" })
+	)(data)
+
+export const DeleteUserAccount = (props) => {
+	const { data } = useGetUsersQuery(props.selectedBusinessId)
+	const { colors } = useContext(ThemeContext)
+
+	const [deleteUser, result] = useDeleteUserMutation()
+	const [userToDelete, setUserToDelete] = useState("")
+	const [deleteModal, setDeleteModal] = useState({ obj: {}, status: false })
+
+	const handleSubmit = (val) => {
+		setDeleteModal((value) => ({ ...value, status: false }))
+		deleteUser({
+			selectedBusinessId: val.obj.selectedBusinessId,
+			email: userToDelete,
+			users: data,
+		})
+	}
+
+	const openModal = (e, selectedBusinessId) => {
+		e.preventDefault()
+		setDeleteModal({ obj: { e, selectedBusinessId }, status: true })
+	}
+
+	return (
+		<>
+			<DivWrapper>
+				<DivWrapper bottom={1}>
+					<Text> Delete a user from this business </Text>
+				</DivWrapper>
+				{data ? (
+					<>
+						<DivWrapper gap={2} max="xs">
+							<Select
+								options={usersDropDown(data, props.user.email)}
+								label=""
+								onChange={(e) => setUserToDelete(e.target.value)}
+								value={userToDelete}
+							/>
+
+							<ButtonState
+								loading={result.isPending}
+								loadingText="Loading"
+								condition={userToDelete}
+								onClick={(e) => openModal(e, props.selectedBusinessId)}
+							>
+								Delete
+							</ButtonState>
+						</DivWrapper>
+					</>
+				) : null}
+			</DivWrapper>
+
+			{deleteModal.status && (
+				<Modal
+					title="Are you sure?"
+					handleClose={() => setDeleteModal({ ...deleteModal, status: false })}
+					noBgClose
+				>
+					<DivWrapper align="center" gap={2}>
+						<Text justify="center" color={colors.red}>
+							Are you sure you want to delete this user
+						</Text>
+						<Button onClick={() => handleSubmit(deleteModal)}>
+							Yes, delete
+						</Button>
+					</DivWrapper>
+				</Modal>
+			)}
+		</>
+	)
 }
