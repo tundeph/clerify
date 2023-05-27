b12ad3c13bff1df412456b4bc79870ada370207b
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-27 17:16:39 +0100

    Update keyword.js

--- src/components/keyword.js
+++ src/components/keyword.js
@@ -4,31 +4,32 @@ import styled from "styled-components"
 import { CustomCloseIcon } from "../layout/styles"
 
 const KeywordButton = styled.div`
-	display: grid;
-	grid-template-columns: 1fr 10px;
-	justify-self: center;
-	align-items: center;
-	min-width: 50px;
-	padding: 0.5em 2.5em;
-	border-radius: 100px;
-	background-color: ${({ theme }) => theme.colors.background};
-	font-size: 0.8rem;
-	color: ${({ theme }) => theme.colors.secondary};
-	box-shadow: 0px 0px 15px ${({ theme }) => theme.colors.gray300};
+  display: grid;
+  grid-template-columns: 1fr 10px;
+  justify-self: center;
+  align-items: center;
+  min-width: 50px;
+  padding: 0.5em 2.5em;
+  border-radius: 100px;
+  background-color: ${({ theme }) => theme.colors.background};
+  font-size: 0.8rem;
+  color: ${({ theme }) => theme.colors.secondary};
+  box-shadow: 0px 0px 15px ${({ theme }) => theme.colors.gray300};
 `
 
-const Keyword = ({ text, onClick }) => {
-	const handleClick = (text) => {
-		if (!onClick) return null
-		return onClick(text)
-	}
+const Keyword = ({ text, Icon, onClick }) => {
+  const handleClick = (text) => {
+    if (!onClick) return null
+    return onClick(text)
+  }
 
-	return (
-		<KeywordButton>
-			{text}
-			{onClick ? <CustomCloseIcon onClick={() => handleClick(text)} /> : null}
-		</KeywordButton>
-	)
+  const IconComponent = Icon ? Icon : CustomCloseIcon
+  return (
+    <KeywordButton>
+      {text}
+      {onClick ? <IconComponent onClick={() => handleClick(text)} /> : null}
+    </KeywordButton>
+  )
 }
 
 export default Keyword
