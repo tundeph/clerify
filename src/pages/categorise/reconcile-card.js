2039b3682d19ef6e77b87c1c91d6e1b291ba1ca7
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-27 13:32:48 +0100

    Update reconcile-card.js

--- src/pages/categorise/reconcile-card.js
+++ src/pages/categorise/reconcile-card.js
@@ -5,26 +5,26 @@ import { DivWrapper, Text, BouncingCabinetIcon } from "../../layout/styles"
 import ButtonState from "@components/button-state"
 
 const ReconcileCard = ({ isPending, onClick }) => {
-	const { colors } = useContext(ThemeContext)
+  const { colors } = useContext(ThemeContext)
 
-	return (
-		<DivWrapper align="center" gap={2}>
-			<Text color={colors.gray300}>
-				<BouncingCabinetIcon size={80} />
-			</Text>
-			<Text align="center">
-				Click the button below to reconcile your transactions
-			</Text>
-			<ButtonState
-				loading={isPending}
-				condition={true}
-				loadingText=""
-				onClick={onClick}
-			>
-				Reconcile
-			</ButtonState>
-		</DivWrapper>
-	)
+  return (
+    <DivWrapper align="center" gap={2}>
+      <Text color={colors.gray300}>
+        <BouncingCabinetIcon size={80} />
+      </Text>
+      <Text align="center">
+        Click the button below to reconcile your transactions
+      </Text>
+      <ButtonState
+        loading={isPending}
+        condition={true}
+        loadingText=""
+        onClick={onClick}
+      >
+        Reconcile
+      </ButtonState>
+    </DivWrapper>
+  )
 }
 
 export default ReconcileCard
