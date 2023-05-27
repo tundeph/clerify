30879579e2501eb6813447e8b909bdf38448d325
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-27 13:32:43 +0100

    Update categorise-intro.js

--- src/pages/categorise/categorise-intro.js
+++ src/pages/categorise/categorise-intro.js
@@ -1,26 +1,28 @@
 import React from "react"
 
-import { Text, Card, Divider, Button } from "../../layout/styles"
+import { DivWrapper, Text, Card, Divider, Button } from "../../layout/styles"
 import { size } from "../../layout/theme"
 
 const CategoriseIntro = ({ sorted, onClick }) => {
-	return (
-		<Card>
-			<Text size={size.xs} bold>
-				{`Well, you still have ${sorted.length}  uncategorized transactions`}
-			</Text>
+  return (
+    <DivWrapper max="s">
+      <Card>
+        <Text size={size.xs} bold>
+          {`Well, you still have ${sorted.length}  uncategorized transactions`}
+        </Text>
 
-			<Text size={size.xxs} bottom={1}>
-				You can reconcile all your transactions by categorising few of them and
-				then allowing the app to do the rest for you
-			</Text>
-			<Divider />
-			<Text bottom={1}>
-				Would you like to manually categorize some of your transactions now?
-			</Text>
-			<Button onClick={onClick}>Categorize now</Button>
-		</Card>
-	)
+        <Text size={size.xxs} bottom={1}>
+          You can reconcile all your transactions by categorising few of them
+          and then allowing the app to do the rest for you
+        </Text>
+        <Divider />
+        <Text bottom={1}>
+          Would you like to manually categorize some of your transactions now?
+        </Text>
+        <Button onClick={onClick}>Categorize now</Button>
+      </Card>
+    </DivWrapper>
+  )
 }
 
 export default CategoriseIntro
