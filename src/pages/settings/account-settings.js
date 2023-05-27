d19fb48e0c8a556b303bd7cdd081a69ec8cb863d
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-27 13:32:56 +0100

    Update account-settings.js

--- src/pages/settings/account-settings.js
+++ src/pages/settings/account-settings.js
@@ -2,36 +2,39 @@ import React from "react"
 import { size } from "../../layout/theme"
 import { Outlet } from "react-router-dom"
 import {
-	PageWrapper,
-	UserWrapper,
-	DivWrapper,
-	Title,
-	Divider,
+  PageWrapper,
+  UserWrapper,
+  DivWrapper,
+  Title,
+  Divider,
 } from "../../layout/styles"
 
 import CustomNavLink from "../../components/custom-navlink"
 
 export const AccountSettings = () => {
-	return (
-		<PageWrapper>
-			<UserWrapper>
-				<DivWrapper bottom={size.xxxs}>
-					<Title> Settings </Title>
-					<DivWrapper>
-						<DivWrapper direction="row" gap={1.5} top={2}>
-							<CustomNavLink to="/account-settings">
-								Edit financial account
-							</CustomNavLink>
-							<CustomNavLink to="/account-settings/add">
-								Add new financial account
-							</CustomNavLink>
-						</DivWrapper>
-						<Divider />
-					</DivWrapper>
-				</DivWrapper>
+  return (
+    <PageWrapper>
+      <UserWrapper>
+        <DivWrapper bottom={size.xxxs}>
+          <Title> Settings </Title>
+          <DivWrapper>
+            <DivWrapper direction="row" gap={1.5} top={2}>
+              <CustomNavLink to="/account-settings">
+                Edit financial account
+              </CustomNavLink>
+              <CustomNavLink to="/account-settings/add">
+                Add new financial account
+              </CustomNavLink>
+              <CustomNavLink to="/account-settings/category">
+                Category settings
+              </CustomNavLink>
+            </DivWrapper>
+            <Divider />
+          </DivWrapper>
+        </DivWrapper>
 
-				<Outlet />
-			</UserWrapper>
-		</PageWrapper>
-	)
+        <Outlet />
+      </UserWrapper>
+    </PageWrapper>
+  )
 }
