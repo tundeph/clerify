2b7ff18840a2b3800916f51a20db44888a0106e6
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-27 17:16:51 +0100

    Update index.js

--- src/helper/index.js
+++ src/helper/index.js
@@ -4,216 +4,230 @@ import { Button, DisabledButton, LoadingIcon } from "../layout/styles"
 import * as R from "ramda"
 
 export const handleButtonState = (
-	loading,
-	loadingText,
-	buttonText,
-	condition
+  loading,
+  loadingText,
+  buttonText,
+  condition
 ) => {
-	const { colors } = useContext(ThemeContext)
+  const { colors } = useContext(ThemeContext)
 
-	if (loading) {
-		return (
-			<DisabledButton>
-				{loadingText} <LoadingIcon />
-			</DisabledButton>
-		)
-	} else {
-		if (condition) {
-			return <Button> {buttonText}</Button>
-		} else {
-			return (
-				<DisabledButton color={colors.gray600}> {buttonText} </DisabledButton>
-			)
-		}
-	}
+  if (loading) {
+    return (
+      <DisabledButton>
+        {loadingText} <LoadingIcon />
+      </DisabledButton>
+    )
+  } else {
+    if (condition) {
+      return <Button> {buttonText}</Button>
+    } else {
+      return (
+        <DisabledButton color={colors.gray600}> {buttonText} </DisabledButton>
+      )
+    }
+  }
 }
 
 export const textSorter = (array, type, sortbyAttr) => {
-	const sorted = array.sort((a, b) => {
-		let aWithAttr = ""
-		let bWithAttr = ""
+  const sorted = array.sort((a, b) => {
+    let aWithAttr = ""
+    let bWithAttr = ""
 
-		if (sortbyAttr) {
-			aWithAttr = a[sortbyAttr]
-			bWithAttr = b[sortbyAttr]
-		} else {
-			aWithAttr = a
-			bWithAttr = b
-		}
+    if (sortbyAttr) {
+      aWithAttr = a[sortbyAttr]
+      bWithAttr = b[sortbyAttr]
+    } else {
+      aWithAttr = a
+      bWithAttr = b
+    }
 
-		if (type === "asc") {
-			if (aWithAttr.toLowerCase() > bWithAttr.toLowerCase()) {
-				return 1
-			} else if (aWithAttr.toLowerCase() < bWithAttr.toLowerCase()) {
-				return -1
-			} else {
-				return 0
-			}
-		} else if (type === "desc") {
-			if (aWithAttr.toLowerCase() > bWithAttr.toLowerCase()) {
-				return -1
-			} else if (aWithAttr.toLowerCase() < bWithAttr.toLowerCase()) {
-				return 1
-			} else {
-				return 0
-			}
-		} else {
-			return array
-		}
-	})
+    if (type === "asc") {
+      if (aWithAttr.toLowerCase() > bWithAttr.toLowerCase()) {
+        return 1
+      } else if (aWithAttr.toLowerCase() < bWithAttr.toLowerCase()) {
+        return -1
+      } else {
+        return 0
+      }
+    } else if (type === "desc") {
+      if (aWithAttr.toLowerCase() > bWithAttr.toLowerCase()) {
+        return -1
+      } else if (aWithAttr.toLowerCase() < bWithAttr.toLowerCase()) {
+        return 1
+      } else {
+        return 0
+      }
+    } else {
+      return array
+    }
+  })
 
-	return sorted
+  return sorted
 }
 
 export const getKeywordsFromRemarks = (remarks) => {
-	//CONSIDER FILTERING OUT GENERIC WORDS LIKE NIP, CR etc to make it smarter
-	//split into 1-word keywords
-	let keywordsArray = remarks.trim().split(/\/| |-/)
-	// add 2-word keywords
-	const twoWordKeywords = []
-	for (let counter = 0; counter <= keywordsArray.length - 1; counter++) {
-		if (keywordsArray[counter] && keywordsArray[counter + 1]) {
-			twoWordKeywords.push(
-				`${keywordsArray[counter]} ${keywordsArray[counter + 1]}`
-			)
-		}
-	}
-	// add 3-word keywords
-	const threeWordKeywords = []
-	for (
-		let counter = 0;
-		counter <= Math.abs(keywordsArray.length / 2);
-		counter++
-	) {
-		if (
-			keywordsArray[counter] &&
-			keywordsArray[counter + 1] &&
-			keywordsArray[counter + 2]
-		) {
-			threeWordKeywords.push(
-				`${keywordsArray[counter]} ${keywordsArray[counter + 1]} ${
-					keywordsArray[counter + 2]
-				}`
-			)
-		}
-	}
+  //split into 1-word keywords
+  let keywordsArray = remarks.trim().split(/\/| |-/)
+  // add 2-word keywords
+  const twoWordKeywords = []
+  for (let counter = 0; counter <= keywordsArray.length - 1; counter++) {
+    if (keywordsArray[counter] && keywordsArray[counter + 1]) {
+      twoWordKeywords.push(
+        `${keywordsArray[counter]} ${keywordsArray[counter + 1]}`
+      )
+    }
+  }
 
-	keywordsArray = [...keywordsArray, ...twoWordKeywords, ...threeWordKeywords]
+  // add 3-word keywords
+  const threeWordKeywords = []
+  for (
+    let counter = 0;
+    counter <= Math.abs(keywordsArray.length / 2);
+    counter++
+  ) {
+    if (
+      keywordsArray[counter] &&
+      keywordsArray[counter + 1] &&
+      keywordsArray[counter + 2]
+    ) {
+      threeWordKeywords.push(
+        `${keywordsArray[counter]} ${keywordsArray[counter + 1]} ${
+          keywordsArray[counter + 2]
+        }`
+      )
+    }
+  }
 
-	return keywordsArray
+  keywordsArray = [...keywordsArray, ...twoWordKeywords, ...threeWordKeywords]
+
+  return keywordsArray
 }
 
 export const reconcileAccts = (categories, accounts) => {
-	if (categories.length === 0) return accounts
+  if (categories.length === 0) return accounts
 
-	/// new logic
-	accounts.forEach((account, index) => {
-		if (!account.categoryId) {
-			for (const category of categories) {
-				let foundCategory = false
+  /// new logic
+  accounts.forEach((account, index) => {
+    if (!account.categoryId) {
+      for (const category of categories) {
+        let foundCategory = false
 
-				if (category.type === account.type) {
-					category.keywords.forEach((selected) => {
-						if (
-							account.remarks
-								.toLowerCase()
-								.includes(selected.keyword.toLowerCase()) &&
-							account.remarks
-								.toLowerCase()
-								.includes(selected.thirdParty.toLowerCase())
-						) {
-							accounts[index].categoryId = category.categoryId
-							foundCategory = true
-						}
-					})
-				}
+        if (category.type === account.type) {
+          category.keywords.forEach((selected) => {
+            if (
+              account.remarks
+                .toLowerCase()
+                .includes(selected.keyword.toLowerCase()) &&
+              account.remarks
+                .toLowerCase()
+                .includes(selected.thirdParty.toLowerCase())
+            ) {
+              accounts[index].categoryId = category.categoryId
+              foundCategory = true
+            }
+          })
+        }
 
-				if (foundCategory) {
-					break
-				}
-			}
-		}
-	})
-	return accounts
+        if (foundCategory) {
+          break
+        }
+      }
+    }
+  })
+  return accounts
 }
 
 export const formatDropDown = (data, selectDescription, sortByProp) => {
-	const sortedData = textSorter([...new Set(data)], "asc", sortByProp)
-		.filter((filteredData) => filteredData.trim().length > 0)
-		.map((selectedData) => ({
-			value: selectedData,
-			label: selectedData,
-		}))
-	const finalData = [{ value: "", label: selectDescription }, ...sortedData]
-	return finalData
+  const sortedData = textSorter([...new Set(data)], "asc", sortByProp)
+    .filter((filteredData) => filteredData.trim().length > 0)
+    .map((selectedData) => ({
+      value: selectedData,
+      label: selectedData,
+    }))
+  const finalData = [{ value: "", label: selectDescription }, ...sortedData]
+  return finalData
 }
 
 export const formatCategory = (transactionCategories) => {
-	const sortedCategories = textSorter(
-		[...transactionCategories],
-		"asc",
-		"title"
-	).map((category) => ({
-		value: category.categoryId,
-		label: category.title,
-	}))
-	return sortedCategories
+  const sortedCategories = textSorter(
+    [...transactionCategories],
+    "asc",
+    "title"
+  ).map((category) => ({
+    value: category.categoryId,
+    label: category.title,
+  }))
+  return sortedCategories
 }
 
 export const formatCategoryDropDown = (transactionCategories) => {
-	const sortedCategories = formatCategory(transactionCategories)
-	const categories = [
-		{ value: "", label: "Select a transaction category" },
-		...sortedCategories,
-	]
-	return categories
+  const sortedCategories = formatCategory(transactionCategories)
+  const categories = [
+    { value: "", label: "Select a transaction category" },
+    ...sortedCategories,
+  ]
+  return categories
 }
 
 export const formatKeywordsDropDown = (uncategorisedAccts) => {
-	const keywordsArray = getKeywordsFromRemarks(uncategorisedAccts)
-	return formatDropDown(keywordsArray, "Select ")
+  const keywordsArray = getKeywordsFromRemarks(uncategorisedAccts)
+  return formatDropDown(keywordsArray, "Select ")
 }
 
 export const formatUpdatedCategories = (
-	categories,
-	category,
-	keyword,
-	thirdParty
+  categories,
+  category,
+  keyword,
+  thirdParty
 ) => {
-	return categories.reduce((acc, item) => {
-		if (item.categoryId === category) {
-			if (
-				!item.keywords.filter(
-					(selected) =>
-						selected.keyword.toLowerCase() === keyword.toLowerCase() &&
-						selected.thirdParty.toLowerCase() === thirdParty.toLowerCase()
-				).length
-			) {
-				item.keywords.push({ keyword, thirdParty })
-			}
-			acc.push(item)
-		} else {
-			acc.push(item)
-		}
-		return acc
-	}, [])
+  return categories.reduce((acc, item) => {
+    if (item.categoryId === category) {
+      if (
+        !item.keywords.filter(
+          (selected) =>
+            selected.keyword.toLowerCase() === keyword.toLowerCase() &&
+            selected.thirdParty.toLowerCase() === thirdParty.toLowerCase()
+        ).length
+      ) {
+        item.keywords.push({ keyword, thirdParty })
+      }
+      acc.push(item)
+    } else {
+      acc.push(item)
+    }
+    return acc
+  }, [])
 }
 
+// adds a new category object into categories array
+export const addCategoryData = (id, business, category) =>
+  R.over(R.lensPath([id, "categories"]), R.append(category), business)
+
+// updates the title of a category object referenced by title
+export const updateCategoryTitle = (id, data, categoryId, title) =>
+  R.evolve({
+    [id]: {
+      categories: R.map(
+        R.when(R.propEq(categoryId, "categoryId"), R.assoc("title", title))
+      ),
+    },
+  })(data)
+
 export const currencyFormatter = (number) =>
-	new Intl.NumberFormat("en-NG", { style: "currency", currency: "NGN" }).format(
-		number
-	)
+  new Intl.NumberFormat("en-NG", { style: "currency", currency: "NGN" }).format(
+    number
+  )
 
 export const findByProp = (prop, propValue, returnProp) =>
-	R.pipe(R.find(R.propEq(prop, propValue)), R.prop(returnProp))
+  R.pipe(R.find(R.propEq(prop, propValue)), R.prop(returnProp))
 
 export const screenSizes = {
-	xxs: "320px",
-	xs: "480px",
-	s: "600px",
-	m: "768px",
-	l: "900px",
-	xl: "1024px",
+  xxs: "320px",
+  xs: "480px",
+  s: "600px",
+  m: "768px",
+  l: "900px",
+  xl: "1024px",
 }
 
 export const TEST_SECRET_KEY = "test_sk_C1d7PNuoqToOVzoSXvHQ"
