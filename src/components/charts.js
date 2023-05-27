a8c310c13f3dae5fbe1b039e57d507a7271191c2
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-28 00:40:07 +0100

    Update charts.js

--- src/components/charts.js
+++ src/components/charts.js
@@ -1,96 +1,91 @@
 import React, { memo } from "react"
 import {
-	Chart as ChartJS,
-	CategoryScale,
-	LinearScale,
-	PointElement,
-	LineElement,
-	ArcElement,
-	BarElement,
-	Title,
-	Tooltip,
-	Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  ArcElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
 } from "chart.js"
 import { Line, Pie, Bar } from "react-chartjs-2"
 
 export const LineChart = memo(({ data }) => {
-	ChartJS.register(
-		CategoryScale,
-		LinearScale,
-		PointElement,
-		LineElement,
-		Title,
-		Tooltip,
-		Legend
-	)
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  )
 
-	const options = {
-		responsive: true,
-		plugins: {
-			legend: {
-				position: "bottom",
-				labels: { boxWidth: 10, boxHeight: 2 },
-			},
-			title: {
-				display: true,
-				// text: "Chart.js Line Chart",
-			},
-		},
-		elements: {
-			line: {
-				borderWidth: 2,
-			},
-			point: {
-				pointStyle: "circle",
-				radius: 0,
-				borderWidth: 0,
-			},
-		},
-	}
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+        labels: {
+          boxWidth: 5,
+          boxHeight: 5,
+        },
+      },
+    },
+    elements: {
+      line: {
+        borderWidth: 2,
+      },
+      point: {
+        pointStyle: "circle",
+        radius: 0,
+        borderWidth: 0,
+      },
+    },
+  }
 
-	return <Line options={options} data={data} />
+  return <Line options={options} data={data} />
 })
 
 export const PieChart = memo(({ data }) => {
-	ChartJS.register(ArcElement, Tooltip, Legend)
+  ChartJS.register(ArcElement, Tooltip, Legend)
 
-	const options = {
-		cutout: "50%",
-		radius: "70%",
-		plugins: {
-			legend: {
-				position: "bottom",
-				labels: { boxWidth: 10, boxHeight: 4 },
-			},
-		},
-	}
+  const options = {
+    cutout: "50%",
+    radius: "70%",
+    plugins: {
+      legend: {
+        position: "bottom",
+        labels: { boxWidth: 10, boxHeight: 10 },
+      },
+    },
+  }
 
-	return <Pie options={options} data={data} />
+  return <Pie options={options} data={data} />
 })
 
 export const BarChart = memo(({ data }) => {
-	ChartJS.register(
-		CategoryScale,
-		LinearScale,
-		BarElement,
-		Title,
-		Tooltip,
-		Legend
-	)
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  )
 
-	const options = {
-		responsive: true,
-		plugins: {
-			legend: {
-				position: "bottom",
-				labels: { boxWidth: 10, boxHeight: 4 },
-			},
-			title: {
-				display: true,
-				// text: 'Chart.js Bar Chart',
-			},
-		},
-	}
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+        labels: { boxWidth: 8, boxHeight: 8 },
+      },
+    },
+  }
 
-	return <Bar options={options} data={data} />
+  return <Bar options={options} data={data} />
 })
