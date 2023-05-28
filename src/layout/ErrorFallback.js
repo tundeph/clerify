0fc1b2910c52a1f23f5f85e070bdf6a18dba04e7
tundeph <54335828+tundeph@users.noreply.github.com>
2023-05-28 21:36:32 +0100

    Update ErrorFallback.js

--- src/layout/ErrorFallback.js
+++ src/layout/ErrorFallback.js
@@ -1,17 +1,26 @@
 "use client"
 
-import React from "react"
+import React, { useState } from "react"
+import { Modal } from "@components"
 
 export const ErrorModal = () => <div>There was an error</div>
 
 export function ErrorFallback({ error, resetErrorBoundary }) {
+  const [errorModal, setErrorModal] = useState(true)
   // Call resetErrorBoundary() to reset the error boundary and retry the render.
   console.log("There is an error")
 
   return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
-    </div>
+    <>
+      {errorModal ? (
+        <Modal
+          role="alert"
+          title="Error"
+          handleClose={() => setErrorModal(false)}
+        >
+          <p>Something went wrong</p>
+        </Modal>
+      ) : null}
+    </>
   )
 }
