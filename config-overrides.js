7183c359c757518693d7d59079c870251070fb2c
tundeph <54335828+tundeph@users.noreply.github.com>
2023-07-08 13:13:52 +0100

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,27 +1,27 @@
-const path = require("path")
+// const path = require("path")
 const { alias } = require("react-app-rewire-alias")
 
-// module.exports = function override(config) {
-//   alias({
-//     "@services": "src/services",
-//     "@components": "src/components",
-//     "@utils": "src/helper",
-//     "@layout": "src/layout",
-//   })(config)
-
-//   return config
-// }
-
 module.exports = function override(config) {
-  config.resolve = {
-    ...config.resolve,
-    alias: {
-      ...config.alias,
-      "@services/*": path.resolve(__dirname, "src/services/*"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@utils/*": path.resolve(__dirname, "src/helper/*"),
-      "@layout/*": path.resolve(__dirname, "src/layout/*"),
-    },
-  }
+  alias({
+    "@services": "src/services",
+    "@components": "src/components",
+    "@utils": "src/helper",
+    "@layout": "src/layout",
+  })(config)
+
   return config
 }
+
+// module.exports = function override(config) {
+//   config.resolve = {
+//     ...config.resolve,
+//     alias: {
+//       ...config.alias,
+//       "@services/*": path.resolve(__dirname, "src/services/*"),
+//       "@components": path.resolve(__dirname, "src/components"),
+//       "@utils/*": path.resolve(__dirname, "src/helper/*"),
+//       "@layout/*": path.resolve(__dirname, "src/layout/*"),
+//     },
+//   }
+//   return config
+// }
