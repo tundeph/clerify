6470369f81ec91334f5d97ad2252b1e0dcceb369
tundeph <54335828+tundeph@users.noreply.github.com>
2023-07-15 17:27:51 +0100

    Update icon-pill.js

--- src/components/icon-pill.js
+++ src/components/icon-pill.js
@@ -1,24 +1,25 @@
+// custom component to render pills used for keywords and other items in the apps
 import React from "react"
 import styled from "styled-components"
 
 import { Pill, DivWrapper } from "@layout/styles"
 
 const CustomDivWrapper = styled(DivWrapper)`
-	display: inline;
+  display: inline;
 `
 const Wrapper = styled.span`
-	display: flex;
-	gap: 8px;
-	align-items: center;
+  display: flex;
+  gap: 8px;
+  align-items: center;
 `
 
 export const IconPill = ({ text, Icon, ...props }) => (
-	<CustomDivWrapper>
-		<Pill {...props}>
-			<Wrapper>
-				<Icon />
-				{text}
-			</Wrapper>
-		</Pill>
-	</CustomDivWrapper>
+  <CustomDivWrapper>
+    <Pill {...props}>
+      <Wrapper>
+        <Icon />
+        {text}
+      </Wrapper>
+    </Pill>
+  </CustomDivWrapper>
 )
