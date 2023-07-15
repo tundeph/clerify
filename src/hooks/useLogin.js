15a18114343d2a19827406af20f6fb586cabf689
tundeph <54335828+tundeph@users.noreply.github.com>
2023-07-15 17:28:58 +0100

    Update useLogin.js

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -4,51 +4,51 @@ import { authService } from "../firebase/config"
 import { useProfileQuery } from "../services/profile-slice2"
 
 export const useLogin = () => {
-	const { refetch } = useProfileQuery()
-	const [error, setError] = useState(null)
-	const [isPending, setIsPending] = useState(false)
-	const [isCancelled, setIsCancelled] = useState(false)
+  const { refetch } = useProfileQuery()
+  const [error, setError] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [isCancelled, setIsCancelled] = useState(false)
 
-	const login = async (userEmail, userPassword) => {
-		setError(null)
-		setIsPending(true)
+  const login = async (userEmail, userPassword) => {
+    setError(null)
+    setIsPending(true)
 
-		try {
-			//firebase function to log log uers in
-			const res = await authService.signInWithEmailAndPassword(
-				userEmail,
-				userPassword
-			)
-			//if user account is available, refetch the rtkquery to fetch user details
-			if (res.user) refetch()
-			if (!isCancelled) {
-				setError(null)
-				setIsPending(false)
-			}
-		} catch (err) {
-			setError(err.message)
-			setIsPending(false)
-		}
-	}
+    try {
+      //firebase function to log users in
+      const res = await authService.signInWithEmailAndPassword(
+        userEmail,
+        userPassword
+      )
+      //if user account is available, refetch the rtkquery to fetch user details
+      if (res.user) refetch()
+      if (!isCancelled) {
+        setError(null)
+        setIsPending(false)
+      }
+    } catch (err) {
+      setError(err.message)
+      setIsPending(false)
+    }
+  }
 
-	const resetPassword = async (userEmail) => {
-		setError(null)
-		setIsPending(true)
+  const resetPassword = async (userEmail) => {
+    setError(null)
+    setIsPending(true)
 
-		try {
-			//firebase function to send reset password by email
-			await authService.sendPasswordResetEmail(userEmail)
-			setError(null)
-			setIsPending(false)
-		} catch (err) {
-			setError(err.message)
-			setIsPending(false)
-		}
-	}
+    try {
+      //firebase function to send reset password by email
+      await authService.sendPasswordResetEmail(userEmail)
+      setError(null)
+      setIsPending(false)
+    } catch (err) {
+      setError(err.message)
+      setIsPending(false)
+    }
+  }
 
-	useEffect(() => {
-		return () => setIsCancelled(true)
-	}, [])
+  useEffect(() => {
+    return () => setIsCancelled(true)
+  }, [])
 
-	return { error, isPending, login, resetPassword }
+  return { error, isPending, login, resetPassword }
 }
