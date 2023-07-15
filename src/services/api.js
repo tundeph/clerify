175af53ab993bb36529892b3dd646c0c132de14b
tundeph <54335828+tundeph@users.noreply.github.com>
2023-07-15 18:13:00 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
+// the api that hooks redux slice and store
 import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const api = createApi({
-	reducerPath: "api",
-	baseQuery: fakeBaseQuery(),
-	endpoints: () => ({}),
-	tagTypes: ["Profile", "Accounts", "Users"],
+  reducerPath: "api",
+  baseQuery: fakeBaseQuery(),
+  endpoints: () => ({}),
+  tagTypes: ["Profile", "Accounts", "Users"],
 })
